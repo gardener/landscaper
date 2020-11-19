2a65dc34e21bb9c141e10303f446520f5a9d8a62
hendrikKahl <hendrik.kahl@sap.com>
2020-11-19 11:41:55 +0100

    bug fixes in oci types & helm

--- pkg/utils/oci/types.go
+++ pkg/utils/oci/types.go
@@ -143,5 +143,5 @@ func (c *WithConfigurationStruct) ApplyOption(options *Options) {
 	if c.Cache != nil {
 		options.CacheConfig = c.Cache
 	}
-	c.AllowPlainHttp = options.AllowPlainHttp
+	options.AllowPlainHttp = c.AllowPlainHttp
 }
