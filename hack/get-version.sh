892d8e03907dad42df46de6b9b925daa3f42ee8c
Tim Schrodi <tim.schrodi@sap.com>
2021-02-23 15:18:11 +0100

    add automatic blueprint and component descriptor artifact build

--- hack/get-version.sh
+++ hack/get-version.sh
@@ -12,7 +12,10 @@ if [ -n "$TM_GIT_REF" ] ; then
   exit 0
 fi
 
-VERSION="$(cat "$(dirname $0)/../VERSION")"
+SOURCE_PATH="$(dirname $0)/.."
+VERSION="$(cat "${SOURCE_PATH}/VERSION")"
+
+pushd ${SOURCE_PATH} > /dev/null 2>&1
 
 if [ -n "$TM_GIT_SHA" ] ; then
   # running e2e test for a PR, calculate image tag by concatenating VERSION and commit sha.
@@ -24,16 +27,6 @@ if [[ "$VERSION" = *-dev ]] ; then
   VERSION="$VERSION-$(git rev-parse HEAD)"
 fi
 
-# .dockerignore ignores all files unrelevant for build (e.g. example/*) to only copy relevant source files to the build
-# container. Hence, git will always detect a dirty work tree when building in a container (many deleted files).
-# This command filters out all deleted files that are ignored by .dockerignore to only detect changes to relevant files
-# as a dirty work tree.
-# Additionally, it filters out changes to the `VERSION` file, as this is currently the only way to inject the
-# version-to-build in our pipelines (see https://github.com/gardener/cc-utils/issues/431).
-TREE_STATE="$([ -z "$(git status --porcelain 2>/dev/null | grep -vf <(git ls-files --deleted --ignored --exclude-from=.dockerignore) -e 'VERSION')" ] && echo clean || echo dirty)"
-
-if [ "$TREE_STATE" = dirty ] ; then
-  VERSION="$VERSION-dirty"
-fi
+popd > /dev/null 2>&1
 
 echo "$VERSION"
