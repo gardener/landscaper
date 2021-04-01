df71bf0079409487f74c4153b4b07ecfc2d0088c
Johannes Aubart <johannes.aubart@sap.com>
2021-04-01 15:45:29 +0200

    add integration tests for deploy item progressing and aborting timeout detection

--- test/integration/deployitems/pickup_timeout.go
+++ test/integration/deployitems/pickup_timeout.go
@@ -10,9 +10,6 @@ import (
 	"path"
 	"time"
 
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/types"
-
 	"github.com/onsi/ginkgo"
 	g "github.com/onsi/gomega"
 	gs "github.com/onsi/gomega/gstruct"
@@ -25,25 +22,6 @@ import (
 	"github.com/gardener/landscaper/test/utils"
 	"github.com/gardener/landscaper/test/utils/envtest"
 )
-
-// RegisterTests registers all tests of this package
-func RegisterTests(f *framework.Framework) {
-	PickupTimeoutTests(f)
-}
-
-const (
-	waitingForDeployItems   = 5 * time.Second  // how long to wait for the landscaper to create deploy items from the installation
-	deployItemPickupTimeout = 10 * time.Second // the landscaper has to be configured accordingly for this test to work!
-	waitingForFailedState   = 10 * time.Second // how long to wait for the landscaper to set the phase to failed after the pickup timed out
-	resyncTime              = 1 * time.Second  // after which time to check again if the condition was not fulfilled the last time
-)
-
-func namespacedName(meta metav1.ObjectMeta) types.NamespacedName {
-	return types.NamespacedName{
-		Namespace: meta.Namespace,
-		Name:      meta.Name,
-	}
-}
 
 func PickupTimeoutTests(f *framework.Framework) {
 	ginkgo.Describe("Deploy Item Pickup Timeout", func() {
@@ -112,7 +90,7 @@ func PickupTimeoutTests(f *framework.Framework) {
 			g.Eventually(func() lsv1alpha1.DeployItemStatus {
 				utils.ExpectNoError(f.Client.Get(ctx, namespacedName(di.ObjectMeta), di))
 				return di.Status
-			}, waitingForFailedState, resyncTime).Should(gs.MatchFields(gs.IgnoreExtras, gs.Fields{
+			}, waitingForReconcile, resyncTime).Should(gs.MatchFields(gs.IgnoreExtras, gs.Fields{
 				"Phase": g.Equal(lsv1alpha1.ExecutionPhaseFailed),
 				"LastError": gs.PointTo(gs.MatchFields(gs.IgnoreExtras, gs.Fields{
 					"Codes":  g.ContainElement(lsv1alpha1.ErrorTimeout),
@@ -148,7 +126,7 @@ func PickupTimeoutTests(f *framework.Framework) {
 			}, waitingForDeployItems, resyncTime).ShouldNot(g.BeNil(), "unable to fetch deploy item")
 
 			ginkgo.By("verify that deploy item is not timed out")
-			time.Sleep(deployItemPickupTimeout + waitingForFailedState) // wait for a potential timeout to happen
+			time.Sleep(deployItemPickupTimeout + waitingForReconcile) // wait for a potential timeout to happen
 			utils.ExpectNoError(f.Client.Get(ctx, namespacedName(di.ObjectMeta), di))
 			// check that deploy item does not have a pickup timeout
 			g.Expect(di.Status.LastError).To(g.Or(g.BeNil(), gs.MatchFields(gs.IgnoreExtras, gs.Fields{
