d64d8b0b60c7c0c3e093c2bd99bd61903bf7c5f6
Tim Schrodi <tim.schrodi@sap.com>
2021-07-09 16:48:32 +0200

    add execution tests

---
+++ pkg/landscaper/controllers/execution/executions_controller_suite_test.go
@@ -0,0 +1,38 @@
+// SPDX-FileCopyrightText: 2020 SAP SE or an SAP affiliate company and Gardener contributors.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package execution_test
+
+import (
+	"path/filepath"
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/gardener/landscaper/test/utils/envtest"
+)
+
+func TestConfig(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Execution Controller Test Suite")
+}
+
+var (
+	testenv *envtest.Environment
+)
+
+var _ = BeforeSuite(func() {
+	var err error
+	projectRoot := filepath.Join("../../../../")
+	testenv, err = envtest.New(projectRoot)
+	Expect(err).ToNot(HaveOccurred())
+
+	_, err = testenv.Start()
+	Expect(err).ToNot(HaveOccurred())
+})
+
+var _ = AfterSuite(func() {
+	Expect(testenv.Stop()).ToNot(HaveOccurred())
+})
