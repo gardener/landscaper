5f7d7224417fb22929b732b6137a2d0f01f90e8f
Johannes Aubart <johannes.aubart@sap.com>
2021-07-20 17:10:21 +0200

    add integration tests for component descriptor imports

--- test/integration/installations/import-export.go
+++ test/integration/installations/import-export.go
@@ -16,9 +16,13 @@ import (
 	. "github.com/onsi/gomega"
 	. "github.com/onsi/gomega/gstruct"
 	k8sv1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/util/yaml"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
+	cdv2 "github.com/gardener/component-spec/bindings-go/apis/v2"
+
 	lsv1alpha1 "github.com/gardener/landscaper/apis/core/v1alpha1"
+	mockv1alpha1 "github.com/gardener/landscaper/apis/deployer/mock/v1alpha1"
 	kutil "github.com/gardener/landscaper/pkg/utils/kubernetes"
 	"github.com/gardener/landscaper/test/framework"
 	"github.com/gardener/landscaper/test/utils"
@@ -56,16 +60,38 @@ func ImportExportTests(f *framework.Framework) {
 
 		It("should pass imports/exports correctly to/from subinstallations", func() {
 			By("Create secrets and targets")
+			// dummy secret
 			secret := &k8sv1.Secret{}
 			utils.ExpectNoError(utils.ReadResourceFromFile(secret, path.Join(testdataDir, "10-dummy-secret.yaml")))
 			secret.SetNamespace(state.Namespace)
 			utils.ExpectNoError(state.Create(ctx, f.Client, secret))
 			expectedDataExport := string(secret.Data["value"])
+			// dummy target
 			target := &lsv1alpha1.Target{}
 			utils.ExpectNoError(utils.ReadResourceFromFile(target, path.Join(testdataDir, "10-dummy-target.yaml")))
 			target.SetNamespace(state.Namespace)
 			utils.ExpectNoError(state.Create(ctx, f.Client, target))
 			expectedTargetExport := target.Spec
+			// component descriptor secret
+			secret2 := &k8sv1.Secret{}
+			utils.ExpectNoError(utils.ReadResourceFromFile(secret2, path.Join(testdataDir, "10-cdimport-secret.yaml")))
+			secret2.SetNamespace(state.Namespace)
+			utils.ExpectNoError(state.Create(ctx, f.Client, secret2))
+			tmpData := secret2.Data["componentDescriptor"]
+			tmpDataJSON, err := yaml.ToJSON(tmpData)
+			utils.ExpectNoError(err)
+			secretCD := &cdv2.ComponentDescriptor{}
+			utils.ExpectNoError(json.Unmarshal(tmpDataJSON, secretCD))
+			// component descriptor configmap
+			cm := &k8sv1.ConfigMap{}
+			utils.ExpectNoError(utils.ReadResourceFromFile(cm, path.Join(testdataDir, "10-cdimport-configmap.yaml")))
+			cm.SetNamespace(state.Namespace)
+			utils.ExpectNoError(state.Create(ctx, f.Client, cm))
+			tmpDataString := cm.Data["componentDescriptor"]
+			tmpDataJSON, err = yaml.ToJSON([]byte(tmpDataString))
+			utils.ExpectNoError(err)
+			cmCD := &cdv2.ComponentDescriptor{}
+			utils.ExpectNoError(json.Unmarshal(tmpDataJSON, cmCD))
 
 			By("Create root installation")
 			root := &lsv1alpha1.Installation{}
@@ -179,6 +205,58 @@ func ImportExportTests(f *framework.Framework) {
 			tlImport = &lsv1alpha1.TargetList{}
 			utils.ExpectNoError(f.Client.List(ctx, tlImport, client.InNamespace(state.Namespace), client.MatchingLabels(labels)))
 			Expect(tlImport.Items).To(HaveLen(0))
+
+			// component descriptor imports
+			// exporting component descriptors is not possible and checking for in-cluster objects doesn't work either
+			// therefore the imports are rendered into the deploy item config
+			By("fetch deploy item provider status")
+			// fetch execution
+			exec := &lsv1alpha1.Execution{}
+			utils.ExpectNoError(f.Client.Get(ctx, subinst.Status.ExecutionReference.NamespacedName(), exec))
+			// there should be exactly one deploy item
+			Expect(exec.Status.DeployItemReferences).To(HaveLen(1))
+			Expect(exec.Status.DeployItemReferences[0].Name).To(BeEquivalentTo("submain-import"))
+			di := &lsv1alpha1.DeployItem{}
+			utils.ExpectNoError(f.Client.Get(ctx, exec.Status.DeployItemReferences[0].Reference.NamespacedName(), di))
+			// extract provider configuration from deploy item
+			conf := &mockv1alpha1.ProviderConfiguration{}
+			utils.ExpectNoError(json.Unmarshal(di.Spec.Configuration.Raw, conf))
+			// extract ProviderStatus field from configuration
+			providerStatusDef := map[string]json.RawMessage{}
+			utils.ExpectNoError(json.Unmarshal(conf.ProviderStatus.Raw, &providerStatusDef))
+
+			By("verify component descriptor imports")
+			// provider status should contain component descriptor imports
+			//  secret ref
+			cdImportBySecretRaw, ok := providerStatusDef["cdImportBySecretRef"]
+			Expect(ok).To(BeTrue(), "cdImportBySecretRef not found in provider status definition")
+			cdImportBySecret := &cdv2.ComponentDescriptor{}
+			utils.ExpectNoError(json.Unmarshal(cdImportBySecretRaw, cdImportBySecret))
+			Expect(cdImportBySecret).To(Equal(secretCD))
+			//  configmap ref
+			cdImportByConfigMapRaw, ok := providerStatusDef["cdImportByConfigMapRef"]
+			Expect(ok).To(BeTrue(), "cdImportByConfigMapRef not found in provider status definition")
+			cdImportByConfigMap := &cdv2.ComponentDescriptor{}
+			utils.ExpectNoError(json.Unmarshal(cdImportByConfigMapRaw, cdImportByConfigMap))
+			Expect(cdImportByConfigMap).To(Equal(cmCD))
+
+			By("verify component descriptor list imports")
+			//  cd list import by referencing multiple cd imports
+			cdListImportByCdRefsRaw, ok := providerStatusDef["cdListImportByCdRefs"]
+			Expect(ok).To(BeTrue(), "cdListImportByCdRefs not found in provider status definition")
+			cdListImportByCdRefs := &[]cdv2.ComponentDescriptor{}
+			utils.ExpectNoError(json.Unmarshal(cdListImportByCdRefsRaw, cdListImportByCdRefs))
+			Expect(cdListImportByCdRefs).To(PointTo(HaveLen(2)))
+			Expect(cdListImportByCdRefs).To(PointTo(ContainElement(*secretCD)))
+			Expect(cdListImportByCdRefs).To(PointTo(ContainElement(*cmCD)))
+			//  cd list import by referencing a cd list import
+			cdListImportByListRefRaw, ok := providerStatusDef["cdListImportByListRef"]
+			Expect(ok).To(BeTrue(), "cdListImportByListRef not found in provider status definition")
+			cdListImportByListRef := &[]cdv2.ComponentDescriptor{}
+			utils.ExpectNoError(json.Unmarshal(cdListImportByListRefRaw, cdListImportByListRef))
+			Expect(cdListImportByListRef).To(PointTo(HaveLen(2)))
+			Expect(cdListImportByListRef).To(PointTo(ContainElement(*secretCD)))
+			Expect(cdListImportByListRef).To(PointTo(ContainElement(*cmCD)))
 		})
 
 	})
