ad6c82da983114aec9806e74740ab44f5955b271
Johannes Aubart <johannes.aubart@sap.com>
2021-08-12 09:45:09 +0200

    add integration test for phase propagation

---
+++ test/integration/installations/register.go
@@ -0,0 +1,22 @@
+// SPDX-FileCopyrightText: 2021 SAP SE or an SAP affiliate company and Gardener contributors.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package installations
+
+import (
+	"time"
+
+	"github.com/gardener/landscaper/test/framework"
+)
+
+var (
+	resyncTime  = 1 * time.Second
+	timeoutTime = 30 * time.Second
+)
+
+// RegisterTests registers all tests of this package
+func RegisterTests(f *framework.Framework) {
+	ImportExportTests(f)
+	PhasePropagationTests(f)
+}
