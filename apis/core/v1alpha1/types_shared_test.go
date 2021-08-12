50888face0097099793a0b8b9809dbe430e14f2b
Achim Weigel <achim.weigel@sap.com>
2021-08-12 10:28:41 +0200

    adapt json parser of type AnyJson

---
+++ apis/core/v1alpha1/types_shared_test.go
@@ -0,0 +1,34 @@
+// SPDX-FileCopyrightText: 2020 SAP SE or an SAP affiliate company and Gardener contributors.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package v1alpha1
+
+import (
+	. "github.com/onsi/ginkgo"
+	"github.com/onsi/gomega"
+
+	"sigs.k8s.io/yaml"
+)
+
+var _ = Describe("Shared Types", func() {
+
+	Context("ExportDefinitions", func() {
+		It("parsing null should result in empty raw message", func() {
+
+			executor1 := TemplateExecutor{}
+			result1, err := yaml.Marshal(executor1)
+
+			gomega.Expect(err).ToNot(gomega.HaveOccurred())
+			gomega.Expect(result1).NotTo(gomega.HaveLen(0))
+
+			executor2 := &TemplateExecutor{}
+			err = yaml.Unmarshal(result1, executor2)
+
+			gomega.Expect(err).ToNot(gomega.HaveOccurred())
+			gomega.Expect(executor2.Template.RawMessage).To(gomega.HaveLen(0))
+		})
+
+	})
+
+})
