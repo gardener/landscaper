22e87a8f04765faecda6b506863b2b3071603bdc
achimweigel <achim.weigel@sap.com>
2021-08-12 16:35:03 +0200

    Adapt json parser of type AnyJson (#284)
    * adapt json parser of type AnyJson
    
    * revendor

---
+++ apis/core/v1alpha1/types_suite_test.go
@@ -0,0 +1,17 @@
+// SPDX-FileCopyrightText: 2021 SAP SE or an SAP affiliate company and Gardener contributors.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package v1alpha1
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	"github.com/onsi/gomega"
+)
+
+func TestConfig(t *testing.T) {
+	gomega.RegisterFailHandler(Fail)
+	RunSpecs(t, "Types Testing")
+}
