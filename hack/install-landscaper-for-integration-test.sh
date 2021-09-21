01ee179f4f16655bf8c3085db5a9cb2479138689
Rene Schuenemann <accounts@reshnm.de>
2021-09-21 17:04:05 +0200

    set the image tag correctly

--- hack/install-landscaper-for-integration-test.sh
+++ hack/install-landscaper-for-integration-test.sh
@@ -79,4 +79,4 @@ landscaper:
 fi
 
 export KUBECONFIG="${TM_KUBECONFIG_PATH}/${CLUSTER_NAME}.config"
-helm upgrade --install --create-namespace ls -n ls-system ./charts/landscaper -f /tmp/values.yaml -f /tmp/registry-values.yaml --set "image.tag=${VERSION}"
+helm upgrade --install --create-namespace ls -n ls-system ./charts/landscaper -f /tmp/values.yaml -f /tmp/registry-values.yaml --set "landscaper.image.tag=${VERSION}"
