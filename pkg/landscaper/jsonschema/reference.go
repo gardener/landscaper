55de641fa6fe5921adc133bfddffcce9ecff8d7d
Johannes Aubart <johannes.aubart@sap.com>
2021-12-06 13:25:26 +0100

    split resolve function into smaller functions to reduce complexity

--- pkg/landscaper/jsonschema/reference.go
+++ pkg/landscaper/jsonschema/reference.go
@@ -19,6 +19,7 @@ import (
 	cdv2 "github.com/gardener/component-spec/bindings-go/apis/v2"
 	"github.com/gardener/component-spec/bindings-go/ctf"
 	"github.com/mandelsoft/vfs/pkg/vfs"
+	"github.com/xeipuuv/gojsonschema"
 	"k8s.io/apimachinery/pkg/util/validation/field"
 
 	lsv1alpha1 "github.com/gardener/landscaper/apis/core/v1alpha1"
@@ -47,6 +48,8 @@ func NewReferenceResolver(refCtx *ReferenceContext) *referenceResolver {
 	return &referenceResolver{refCtx}
 }
 
+// Resolve walks through the given json schema and recursively resolves all references which use one of
+// the "local", "blueprint", or "cd" schemes.
 func (rr *referenceResolver) Resolve(schemaBytes []byte) (interface{}, error) {
 	data, err := decodeJSON(schemaBytes)
 	if err != nil {
@@ -58,54 +61,77 @@ func (rr *referenceResolver) Resolve(schemaBytes []byte) (interface{}, error) {
 func (rr *referenceResolver) resolve(data interface{}, currentPath *field.Path) (interface{}, error) {
 	switch typedData := data.(type) {
 	case map[string]interface{}:
-		isRef := false
-		var res interface{}
-		var resMap map[string]interface{}
-		for k, v := range typedData {
-			subPath := currentPath.Child(k)
-			if k == "$ref" {
-				isRef = true
-				uri, ok := v.(string)
-				if !ok {
-					return nil, fmt.Errorf("invalid reference value: expected string, got %v", v)
-				}
-				if len(typedData) > 1 {
-					return nil, fmt.Errorf("invalid reference: fields next to '$ref' are not allowed")
-				}
-				sub, err := rr.resolveReference(uri, subPath)
-				if err != nil {
-					return nil, fmt.Errorf("error resolving reference at %s: %w", subPath, err)
-				}
-				res = sub
-				break
-			}
-			if resMap == nil {
-				resMap = map[string]interface{}{}
-			}
-			sub, err := rr.resolve(v, subPath)
-			if err != nil {
-				return nil, err
-			}
-			resMap[k] = sub
-		}
-		if isRef {
-			return res, nil
-		}
-		return resMap, nil
+		return rr.resolveMap(typedData, currentPath)
 	case []interface{}:
-		resList := make([]interface{}, len(data.([]interface{})))
-		for i, e := range data.([]interface{}) {
-			sub, err := rr.resolve(e, currentPath.Index(i))
-			if err != nil {
-				return nil, err
-			}
-			resList[i] = sub
-		}
-		return resList, nil
+		return rr.resolveList(typedData, currentPath)
 	}
 	return data, nil
 }
 
+// resolveMap is a helper function which can recursively resolve a map
+func (rr *referenceResolver) resolveMap(data map[string]interface{}, currentPath *field.Path) (interface{}, error) {
+	isRef, uri, err := checkForReference(data, currentPath)
+	if err != nil {
+		return err, nil
+	}
+
+	if isRef {
+		// current map is a reference
+		sub, err := rr.resolveReference(uri, currentPath)
+		if err != nil {
+			return nil, fmt.Errorf("error resolving reference at %s: %w", currentPath.Child(gojsonschema.KEY_REF).String(), err)
+		}
+		return sub, nil
+	}
+
+	// current map is not a reference
+	// iterate over entries and resolve each of them
+	res := map[string]interface{}{}
+	for k, v := range data {
+		subPath := currentPath.Child(k)
+		if k == gojsonschema.KEY_REF {
+			// map contains a reference
+			return nil, fmt.Errorf("invalid reference at %s: there are no other fields allowed next to %q", subPath.String(), gojsonschema.KEY_REF)
+		}
+		sub, err := rr.resolve(v, subPath)
+		if err != nil {
+			return nil, err
+		}
+		res[k] = sub
+	}
+	return res, nil
+}
+
+// checkForReference checks whether a given map is a valid reference
+// If it is a reference, it returns true and the URL of the reference.
+// Otherwise, it returns false and an empty string.
+func checkForReference(data map[string]interface{}, currentPath *field.Path) (bool, string, error) {
+	value, ok := data[gojsonschema.KEY_REF]
+	if len(data) != 1 || !ok {
+		// no reference
+		return false, "", nil
+	}
+	typedValue, ok := value.(string)
+	if !ok {
+		return true, "", fmt.Errorf("invalid reference value at %s: expected string, got %v", currentPath.Child(gojsonschema.KEY_REF).String(), value)
+	}
+	return true, typedValue, nil
+}
+
+// resolveList is a helper function which can recursively resolve a list
+func (rr *referenceResolver) resolveList(data []interface{}, currentPath *field.Path) (interface{}, error) {
+	resList := make([]interface{}, len(data))
+	for i, e := range data {
+		sub, err := rr.resolve(e, currentPath.Index(i))
+		if err != nil {
+			return nil, err
+		}
+		resList[i] = sub
+	}
+	return resList, nil
+}
+
+// resolveReference resolves a reference
 func (rr *referenceResolver) resolveReference(s string, currentPath *field.Path) (interface{}, error) {
 	uri, err := url.Parse(s)
 	if err != nil {
@@ -119,10 +145,11 @@ func (rr *referenceResolver) resolveReference(s string, currentPath *field.Path)
 	case "cd":
 		return rr.handleComponentDescriptorReference(uri, currentPath)
 	}
+
 	// unknown reference scheme
 	// rebuild reference because it is replaced in calling method
 	return map[string]interface{}{
-		"$ref": s,
+		gojsonschema.KEY_REF: s,
 	}, nil
 }
 
