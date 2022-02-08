b9e9afedc3f21ed64febff2f959474a4d74171a8
Johannes Aubart <johannes.aubart@sap.com>
2022-02-08 15:52:22 +0100

    order subinstallations by dependencies

--- pkg/landscaper/installations/subinstallations/helper.go
+++ pkg/landscaper/installations/subinstallations/helper.go
@@ -12,6 +12,7 @@ import (
 	"github.com/gardener/component-spec/bindings-go/codec"
 	"github.com/gardener/component-spec/bindings-go/ctf"
 	"github.com/pkg/errors"
+	"k8s.io/apimachinery/pkg/util/sets"
 	"k8s.io/apimachinery/pkg/util/validation/field"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
@@ -106,15 +107,121 @@ func GetBlueprintDefinitionFromInstallationTemplate(
 	return subBlueprint, cdDef, nil
 }
 
+// ComputeSubinstallationDependencies computes the dependencies between the given subinstallations.
+// It checks for every import of every subinstallation, whether that import is exported from another subinstallation.
+// The resulting map contains one entry for every given subinstallation template, mapping that templates name to the
+// set of templates (by name) it depends on.
+func ComputeSubinstallationDependencies(installationTmpl []*lsv1alpha1.InstallationTemplate) map[string]sets.String {
+	dataExports := map[string]string{}
+	targetExports := map[string]string{}
+	for _, tmpl := range installationTmpl {
+		for _, exp := range tmpl.Exports.Data {
+			dataExports[exp.DataRef] = tmpl.Name
+		}
+		for _, exp := range tmpl.Exports.Targets {
+			targetExports[exp.Target] = tmpl.Name
+		}
+	}
+
+	deps := map[string]sets.String{}
+	for _, tmpl := range installationTmpl {
+		tmp := sets.NewString()
+		for _, imp := range tmpl.Imports.Data {
+			if len(imp.DataRef) == 0 {
+				// only dataRef imports can refer to sibling exports
+				continue
+			}
+			source, ok := dataExports[imp.DataRef]
+			if !ok {
+				// no sibling exports this import, it has to come from the parent
+				// this is already checked by validation, no need to verify it here
+				continue
+			}
+			tmp.Insert(source)
+		}
+		for _, imp := range tmpl.Imports.Targets {
+			targets := []string{}
+			if len(imp.Target) != 0 {
+				targets = append(targets, imp.Target)
+			} else if len(imp.Targets) != 0 {
+				targets = imp.Targets
+			} else {
+				// targetListReferences can only refer to parent imports, not to sibling exports
+				continue
+			}
+			for _, t := range targets {
+				source, ok := targetExports[t]
+				if !ok {
+					// no sibling exports this import, it has to come from the parent
+					// this is already checked by validation, no need to verify it here
+					continue
+				}
+				tmp.Insert(source)
+			}
+		}
+		deps[tmpl.Name] = tmp
+	}
+	return deps
+}
+
+// OrderInstallationTemplates takes a list of installation templates and orders them according to their dependencies (as computed by ComputeSubinstallationDependencies).
+// Installation templates which have been put into the new order are referred to as 'scheduled'.
+func OrderInstallationTemplates(installationTmpl []*lsv1alpha1.InstallationTemplate) ([]*lsv1alpha1.InstallationTemplate, error) {
+	// 'done' and 'ordered' more or less contain the same information:
+	// which installation templates have been ordered yet.
+	// 'ordered' is a list to preserve the computed order,
+	// while 'done' is a set to efficiently check whether a specific installation template is already scheduled.
+	ordered := []*lsv1alpha1.InstallationTemplate{}
+	done := sets.NewString()
+	dependencies := ComputeSubinstallationDependencies(installationTmpl)
+	// repeat until either
+	// - all items from installationTmpl have been transferred to ordered (we are finished)
+	// - no new items have been scheduled during the last run
+	//   which hints that all items left are part of a cycle and the dependencies cannot be resolved
+	for oldSize := -1; oldSize != len(done) && len(ordered) != len(installationTmpl); {
+		oldSize = len(done)
+		for _, tmpl := range installationTmpl {
+			// skip if already scheduled
+			if done.Has(tmpl.Name) {
+				continue
+			}
+			// verify that all dependencies of the current subinstallation are already scheduled
+			deps, ok := dependencies[tmpl.Name]
+			if !ok {
+				return nil, fmt.Errorf("subinstallation template %q not found in dependency hierarchy", tmpl.Name)
+			}
+			if len(deps) > len(done) {
+				// installation template has more dependencies than are currently scheduled
+				// so we don't have to check whether all of them are fulfilled because at least one won't be
+				continue
+			}
+			canBeScheduled := true
+			for d := range deps {
+				if !done.Has(d) {
+					// at least one dependency has not been scheduled yet
+					canBeScheduled = false
+					break
+				}
+			}
+			// schedule subinstallation, if possible
+			if canBeScheduled {
+				ordered = append(ordered, tmpl)
+				done.Insert(tmpl.Name)
+			}
+		}
+	}
+	if len(done) != len(installationTmpl) || len(ordered) != len(installationTmpl) { // these comparisons should be equivalent, just checking both to be sure ...
+		// TODO: give more information on cyclic dependency
+		return nil, fmt.Errorf("cyclic dependency detected")
+	}
+	return ordered, nil
+}
+
 // ValidateSubinstallations validates the installation templates in context of the current blueprint.
 func (o *Operation) ValidateSubinstallations(installationTmpl []*lsv1alpha1.InstallationTemplate) error {
-	coreInstTmpls := make([]*core.InstallationTemplate, len(installationTmpl))
-	for i, tmpl := range installationTmpl {
-		coreTmpl := &core.InstallationTemplate{}
-		if err := lsv1alpha1.Convert_v1alpha1_InstallationTemplate_To_core_InstallationTemplate(tmpl, coreTmpl, nil); err != nil {
-			return err
-		}
-		coreInstTmpls[i] = coreTmpl
+	coreInstTmpls, err := convertToCoreInstallationTemplates(installationTmpl)
+	if err != nil {
+		return err
 	}
 
 	coreImports, err := o.buildCoreImports(o.Inst.Blueprint.Info.Imports)
@@ -126,6 +233,19 @@ func (o *Operation) ValidateSubinstallations(installationTmpl []*lsv1alpha1.Inst
 		return o.NewError(allErrs.ToAggregate(), "ValidateSubInstallations", allErrs.ToAggregate().Error())
 	}
 	return nil
+}
+
+// convertToCoreInstallationTemplates converts a list of v1alpha1 InstallationTemplates to their core version
+func convertToCoreInstallationTemplates(installationTmpl []*lsv1alpha1.InstallationTemplate) ([]*core.InstallationTemplate, error) {
+	coreInstTmpls := make([]*core.InstallationTemplate, len(installationTmpl))
+	for i, tmpl := range installationTmpl {
+		coreTmpl := &core.InstallationTemplate{}
+		if err := lsv1alpha1.Convert_v1alpha1_InstallationTemplate_To_core_InstallationTemplate(tmpl, coreTmpl, nil); err != nil {
+			return nil, err
+		}
+		coreInstTmpls[i] = coreTmpl
+	}
+	return coreInstTmpls, nil
 }
 
 // buildCoreImports converts the given list of versioned ImportDefinitions (potentially containing nested conditional imports) into a flattened list of core ImportDefinitions.
