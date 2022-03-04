e69881f2977a052a035c3cc6d11ab4355bae2f49
René Schünemann <rene.schuenemann@sap.com>
2022-03-04 10:30:14 +0100

    make setup-envtest working on darwin/arm64

--- hack/setup-testenv.sh
+++ hack/setup-testenv.sh
@@ -12,7 +12,15 @@ echo "> Setup Test Environment for K8s Version ${K8S_VERSION}"
 
 CURRENT_DIR=$(dirname $0)
 PROJECT_ROOT="${CURRENT_DIR}"/..
-export KUBEBUILDER_ASSETS=$(setup-envtest use -p path ${K8S_VERSION})
+
+# TODO: setup-envtest currently doesnt support darwin/arm64 / force amd64
+ARCH_ARG=""
+if [[ $(go env GOOS) == "darwin" && $(go env GOARCH) == "arm64" ]]; then
+  ARCH_ARG="--arch amd64"
+fi
+
+export KUBEBUILDER_ASSETS=$(setup-envtest use -p path ${K8S_VERSION} ${ARCH_ARG})
+
 mkdir -p ${PROJECT_ROOT}/tmp/test
 rm -f ${PROJECT_ROOT}/tmp/test/bin
 ln -s "${KUBEBUILDER_ASSETS}" ${PROJECT_ROOT}/tmp/test/bin
