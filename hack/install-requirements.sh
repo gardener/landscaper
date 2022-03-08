9a73a56090c3370bc9181ad9a0a95efadd168c0b
René Schünemann <rene.schuenemann@sap.com>
2022-03-08 09:40:32 +0100

    fix installation of test OCI registry on darwin/arm64

--- hack/install-requirements.sh
+++ hack/install-requirements.sh
@@ -9,9 +9,16 @@ set -e
 CURRENT_DIR=$(dirname $0)
 PROJECT_ROOT="${CURRENT_DIR}"/..
 
-curl -sfL "https://install.goreleaser.com/github.com/golangci/golangci-lint.sh" | sh -s -- -b $(go env GOPATH)/bin v1.32.2
+# test OCI registry currently doesnt support darwin/arm64 / force amd64
+ARCH_ARG=$(go env GOARCH)
+if [[ $(go env GOOS) == "darwin" && $(go env GOARCH) == "arm64" ]]; then
+  ARCH_ARG="amd64"
+fi
+
 mkdir -p ${PROJECT_ROOT}/tmp/test/registry
-curl -sfL "https://storage.googleapis.com/gardener-public/test/oci-registry/registry-$(go env GOOS)-$(go env GOARCH)" --output ${PROJECT_ROOT}/tmp/test/registry/registry
+curl -sfL "https://storage.googleapis.com/gardener-public/test/oci-registry/registry-$(go env GOOS)-${ARCH_ARG}" --output ${PROJECT_ROOT}/tmp/test/registry/registry
 chmod +x ${PROJECT_ROOT}/tmp/test/registry/registry
 
 GO111MODULE=off go get golang.org/x/tools/cmd/goimports
+
+curl -sfL "https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh" | sh -s -- -b $(go env GOPATH)/bin v1.32.2
