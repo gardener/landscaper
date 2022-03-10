c0ba7a277bc19d51adba8e1ab672e70555be42ae
René Schünemann <rene.schuenemann@sap.com>
2022-03-10 10:09:14 +0100

    check error

--- pkg/landscaper/installations/executions/operation_test.go
+++ pkg/landscaper/installations/executions/operation_test.go
@@ -89,7 +89,9 @@ var _ = Describe("Execution Operation", func() {
 		}
 
 		inst := testInstallations["test1/root"]
+		Expect(inst).ToNot(BeNil())
 		intBlueprint, err := blueprints.Resolve(ctx, componentResolver, lsCtx.External.ComponentDescriptorRef(), inst.Spec.Blueprint)
+		Expect(err).ToNot(HaveOccurred())
 
 		internalInst, err := installations.New(inst, intBlueprint)
 		Expect(err).ToNot(HaveOccurred())
