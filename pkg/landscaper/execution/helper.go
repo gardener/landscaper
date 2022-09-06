da77e6be17f97d3940620623da67129cd72ef69d
Achim Weigel <achim.weigel@sap.com>
2022-09-06 08:19:21 +0200

    remove old reconcile from executions (run-int-tests)

--- pkg/landscaper/execution/helper.go
+++ pkg/landscaper/execution/helper.go
@@ -6,10 +6,8 @@ package execution
 
 import (
 	"context"
-	"fmt"
 	"strings"
 
-	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/sets"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -17,7 +15,6 @@ import (
 	lsv1alpha1 "github.com/gardener/landscaper/apis/core/v1alpha1"
 	lsv1alpha1helper "github.com/gardener/landscaper/apis/core/v1alpha1/helper"
 	kutil "github.com/gardener/landscaper/controller-utils/pkg/kubernetes"
-	"github.com/gardener/landscaper/controller-utils/pkg/logging"
 	"github.com/gardener/landscaper/pkg/utils/read_write_layer"
 )
 
@@ -80,65 +77,4 @@ func (o *Operation) getExecutionItems(items []lsv1alpha1.DeployItem) ([]*executi
 		}
 	}
 	return execItems, orphaned
-}
-
-// HandleDeployItemPhaseAndGenerationChanges updates the phase of the given execution, if its phase doesn't match the combined phase of its deploy items anymore.
-// If a deploy item's generation differs from the observed one or a deploy item doesn't exist, the phase is set to 'Progressing'.
-// If the phase is changed to 'Succeeded', the exports of the deploy items are updated.
-func (o *Operation) HandleDeployItemPhaseAndGenerationChanges(ctx context.Context) error {
-	logger, ctx := logging.FromContextOrNew(ctx, nil)
-
-	deployitems := []lsv1alpha1.DeployItem{}
-	phases := []lsv1alpha1.ExecutionPhase{}
-	// fetch all managed deploy items and check their phase and generation
-	for _, managedDi := range o.exec.Status.DeployItemReferences {
-		di := &lsv1alpha1.DeployItem{}
-		err := read_write_layer.GetDeployItem(ctx, o.Client(), managedDi.Reference.NamespacedName(), di)
-		if err != nil {
-			if !apierrors.IsNotFound(err) {
-				return fmt.Errorf("unable to get deploy item %q: %w", managedDi.Reference.NamespacedName().String(), err)
-			} else {
-				di = nil
-			}
-		}
-
-		if di == nil || managedDi.Reference.ObservedGeneration != di.Generation {
-			// at least one deploy item is outdated or got deleted, a reconcile is required
-			logger.Debug("deploy item cannot be found or does not match last observed generation")
-			o.exec.Status.Phase = lsv1alpha1.ExecutionPhaseProgressing
-			err := o.Writer().UpdateExecutionStatus(ctx, read_write_layer.W000028, o.exec)
-			if err != nil {
-				return fmt.Errorf("error updating execution status for %s/%s: %w", o.exec.Namespace, o.exec.Name, err)
-			}
-			return nil
-		}
-		phases = append(phases, di.Status.Phase)
-		deployitems = append(deployitems, *di)
-	}
-
-	cp := lsv1alpha1helper.CombinedExecutionPhase(phases...)
-	if o.exec.Status.Phase != cp {
-		// Phase is completed but doesn't fit to the deploy items' phases
-		logger.Debug("execution phase mismatch", "phase", string(o.exec.Status.Phase), "combinedPhase", string(cp))
-		o.exec.Status.Phase = cp
-		err := o.Writer().UpdateExecutionStatus(ctx, read_write_layer.W000030, o.exec)
-		if err != nil {
-			return fmt.Errorf("error updating execution status for %s/%s: %w", o.exec.Namespace, o.exec.Name, err)
-		}
-
-		if cp == lsv1alpha1.ExecutionPhaseSucceeded {
-			// phase changed to Succeeded, it might be necessary to generate the exports again
-			logger.Debug("phase changed to %q, compute deploy item exports again", string(lsv1alpha1.ExecutionPhaseSucceeded))
-			execItems, _ := o.getExecutionItems(deployitems)
-			err = o.collectAndUpdateExports(ctx, execItems)
-			if err != nil {
-				return fmt.Errorf("error while updating exports of execution %s/%s: %w", o.exec.Namespace, o.exec.Name, err)
-			}
-		}
-
-		return nil
-	}
-	logger.Debug("execution is in a final state and deployitems are up-to-date")
-
-	return nil
 }
