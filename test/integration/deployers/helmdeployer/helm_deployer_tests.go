1c7802e8f8d68f823949b2f83a10ab7c86a09715
enrico-kaack-comp <enrico.kaack@sap.com>
2022-10-04 09:19:50 +0200

    use new ginko v2 (run-int-tests)

--- test/integration/deployers/helmdeployer/helm_deployer_tests.go
+++ test/integration/deployers/helmdeployer/helm_deployer_tests.go
@@ -22,7 +22,7 @@ import (
 	"github.com/gardener/landscaper/pkg/deployer/helm"
 	lsutils "github.com/gardener/landscaper/pkg/utils/landscaper"
 
-	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
