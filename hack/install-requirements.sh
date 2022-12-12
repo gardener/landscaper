846455eacb606cae5aaa161d919b443c75a6e05b
Johannes Aubart <johannes.aubart@sap.com>
2022-12-12 16:12:35 +0100

    make install-requirements now installs jq

--- hack/install-requirements.sh
+++ hack/install-requirements.sh
@@ -22,3 +22,18 @@ chmod +x ${PROJECT_ROOT}/tmp/test/registry/registry
 GO111MODULE=off go get golang.org/x/tools/cmd/goimports
 
 curl -sfL "https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh" | sh -s -- -b $(go env GOPATH)/bin v1.49.0
+
+# install jq (needed for documentation index generation)
+if ! jq --version &>/dev/null; then
+  os="linux64"
+  if [[ $(uname -o) == "Darwin" ]]; then
+    os="osx-amd64"
+  fi
+  tmpdir=$(mktemp -d)
+  curl -sfL "https://github.com/stedolan/jq/releases/download/jq-1.6/jq-${os}" --output "${tmpdir}/jq"
+  chmod +x "${tmpdir}/jq"
+  # try to copy to /usr/local/bin, modify PATH as a workaround
+  if ! cp "${tmpdir}/jq" /usr/local/bin/jq >/dev/null; then
+    export PATH=${tmpdir}/jq:$PATH
+  fi
+fi
