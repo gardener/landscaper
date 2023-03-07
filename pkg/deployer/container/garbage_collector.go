a1d37c106634b338e9c33a9985313350c5f2fac9
Johannes Aubart <johannes.aubart@sap.com>
2023-03-07 16:00:03 +0100

    not finding a resource is not an error while garbage collecting (run-int-tests) (#706)

--- pkg/deployer/container/garbage_collector.go
+++ pkg/deployer/container/garbage_collector.go
@@ -154,8 +154,13 @@ func (gc *GarbageCollector) cleanupRBACResources(obj client.Object) reconcile.Re
 
 // cleanupSecret deletes secrets that do not have a parent deploy item anymore.
 func (gc *GarbageCollector) cleanupSecret(ctx context.Context, req reconcile.Request) (reconcile.Result, error) {
+	logger := gc.log.WithValues(lc.KeyResourceKind, "Secret", lc.KeyResource, req.NamespacedName)
 	obj := &corev1.Secret{}
 	if err := gc.hostClient.Get(ctx, req.NamespacedName, obj); err != nil {
+		if apierrors.IsNotFound(err) {
+			logger.Debug("Secret not found, aborting garbage collection")
+			return reconcile.Result{}, nil
+		}
 		return reconcile.Result{}, err
 	}
 	shouldGC, err := gc.shouldGarbageCollect(ctx, obj)
@@ -174,11 +179,15 @@ func (gc *GarbageCollector) cleanupSecret(ctx context.Context, req reconcile.Req
 
 // cleanupPod deletes pods that do not have a parent deploy item anymore.
 func (gc *GarbageCollector) cleanupPod(ctx context.Context, req reconcile.Request) (reconcile.Result, error) {
+	logger := gc.log.WithValues(lc.KeyResourceKind, "Pod", lc.KeyResource, req.NamespacedName)
 	obj := &corev1.Pod{}
 	if err := gc.hostClient.Get(ctx, req.NamespacedName, obj); err != nil {
+		if apierrors.IsNotFound(err) {
+			logger.Debug("Pod not found, aborting garbage collection")
+			return reconcile.Result{}, nil
+		}
 		return reconcile.Result{}, err
 	}
-	logger := gc.log.WithValues(lc.KeyResourceKind, "Pod", lc.KeyResource, kutil.ObjectKeyFromObject(obj).String())
 
 	if obj.Status.Phase == corev1.PodPending || obj.Status.Phase == corev1.PodRunning || obj.Status.Phase == corev1.PodUnknown {
 		logger.Debug("Not garbage collected", lc.KeyReason, "pod is still running", lc.KeyPhase, obj.Status.Phase)
