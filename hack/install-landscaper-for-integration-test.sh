680c2fb17cf6e05bcd409c635e87b0438929e8d3
Johannes Aubart <johannes.aubart@sap.com>
2023-04-17 14:05:28 +0200

    change shell to bash

--- hack/install-landscaper-for-integration-test.sh
+++ hack/install-landscaper-for-integration-test.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #
 # Copyright (c) 2021 SAP SE or an SAP affiliate company. All rights reserved. This file is licensed under the Apache Software License, v. 2 except as noted otherwise in the LICENSE file
 #
