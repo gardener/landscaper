3397f3041cdd36793f11da56002a9d2f78cff3ff
René Schünemann <rene.schuenemann@sap.com>
2023-06-30 16:10:13 +0200

    create dummy cd

---
+++ hack/create-dummy-cd.py
@@ -0,0 +1,27 @@
+import os
+import yaml
+
+version = os.environ["VERSION"]
+repo_ctx = os.environ["REPO_CTX"]
+component_descriptor_file = os.environ["COMPONENT_DESCRIPTOR_FILE"]
+
+cd = {
+    "meta": {
+      "schemaVersion": "v2"
+    },
+    "component": {
+      "name": "github.com/gardener/landscaper",
+      "version": version,
+      "provider": "internal",
+      "repositoryContexts": [
+        {
+          "baseUrl": repo_ctx,
+          "componentNameMapping": "urlPath",
+          "type": "ociRegistry"
+        }
+      ],
+    }
+}
+
+with open(component_descriptor_file, "w+") as cd_file:
+    cd_file.write(yaml.safe_dump(cd))
