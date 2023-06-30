94366c0d05b72c06ab776c5bd2114249cf8d2991
René Schünemann <rene.schuenemann@sap.com>
2023-06-30 16:33:54 +0200

    next try

--- hack/create-dummy-cd.py
+++ hack/create-dummy-cd.py
@@ -3,28 +3,15 @@ import yaml
 
 version = os.environ["VERSION"]
 repo_ctx = os.environ["REPO_CTX"]
-component_descriptor_file = os.environ["COMPONENT_DESCRIPTOR_FILE"]
+component_descriptor_dir = os.environ["COMPONENT_DESCRIPTOR_DIR"]
 
-cd = {
-    "meta": {
-      "schemaVersion": "v2"
-    },
-    "component": {
-      "name": "github.com/gardener/landscaper",
-      "version": version,
-      "provider": "internal",
-      "repositoryContexts": [
-        {
-          "baseUrl": repo_ctx,
-          "componentNameMapping": "urlPath",
-          "type": "ociRegistry"
-        }
-      ],
-      "sources": [],
-      "resources": [],
-      "componentReferences": []
-    }
-}
+cd = None
 
-with open(component_descriptor_file, "w+") as cd_file:
+with open(os.path.join(component_descriptor_dir, "base_component_descriptor_v2"), "r") as base_cd_file:
+    cd = yaml.safe_load(base_cd_file.read())
+
+cd["component"]["version"] = version
+cd["component"]["repositoryContexts"][0]["baseUrl"] = repo_ctx
+
+with open(os.path.join(component_descriptor_dir, "component_descriptor_v2"), "w+") as cd_file:
     cd_file.write(yaml.safe_dump(cd))
