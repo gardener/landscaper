0ba8aebaa1ca1ff90e2fb2e87258abad93b29896
Robert Graeff <robert.graeff@sap.com>
2023-08-01 17:27:56 +0200

    Cleanup (run-int-tests)

--- pkg/deployer/container/helper.go
+++ pkg/deployer/container/helper.go
@@ -5,62 +5,15 @@
 package container
 
 import (
-	"context"
 	"fmt"
 
-	"github.com/gardener/landscaper/pkg/utils/read_write_layer"
-
-	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/serializer"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 
-	"github.com/gardener/landscaper/controller-utils/pkg/logging"
-	lc "github.com/gardener/landscaper/controller-utils/pkg/logging/constants"
-
-	lsv1alpha1 "github.com/gardener/landscaper/apis/core/v1alpha1"
 	containerv1alpha1 "github.com/gardener/landscaper/apis/deployer/container/v1alpha1"
 	"github.com/gardener/landscaper/pkg/api"
-	"github.com/gardener/landscaper/pkg/deployer/lib/targetselector"
 	lsversion "github.com/gardener/landscaper/pkg/version"
 )
-
-func getAndCheckReconcile(ctx context.Context, lsClient client.Client, config containerv1alpha1.Configuration, key client.ObjectKey) (*lsv1alpha1.DeployItem, error) {
-	logger, ctx := logging.FromContextOrNew(ctx, nil, lc.KeyResource, key.String())
-
-	deployItem := &lsv1alpha1.DeployItem{}
-	if err := read_write_layer.GetDeployItem(ctx, lsClient, key, deployItem); err != nil {
-		if apierrors.IsNotFound(err) {
-			logger.Debug(err.Error())
-			return nil, nil
-		}
-		return nil, err
-	}
-
-	if deployItem.Spec.Type != Type {
-		logger.Debug("DeployItem is of wrong type", lc.KeyDeployItemType, deployItem.Spec.Type)
-		return nil, nil
-	}
-
-	if deployItem.Spec.Target != nil {
-		target := &lsv1alpha1.Target{}
-		if err := lsClient.Get(ctx, deployItem.Spec.Target.NamespacedName(), target); err != nil {
-			return nil, fmt.Errorf("unable to get target for deploy item: %w", err)
-		}
-		if len(config.TargetSelector) != 0 {
-			matched, err := targetselector.MatchOne(target, config.TargetSelector)
-			if err != nil {
-				return nil, fmt.Errorf("unable to match target selector: %w", err)
-			}
-			if !matched {
-				logger.Debug("The deploy item's target does not match the given target selector")
-				return nil, nil
-			}
-		}
-	}
-
-	return deployItem, nil
-}
 
 // DefaultConfiguration sets the defaults for the container deployer configuration.
 func DefaultConfiguration(obj *containerv1alpha1.Configuration) {
--- pkg/deployer/container/reconciler_pod.go
+++ pkg/deployer/container/reconciler_pod.go
@@ -6,6 +6,7 @@ package container
 
 import (
 	"context"
+	"fmt"
 
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -20,7 +21,9 @@ import (
 	"github.com/gardener/landscaper/apis/deployer/container"
 	containerv1alpha1 "github.com/gardener/landscaper/apis/deployer/container/v1alpha1"
 	"github.com/gardener/landscaper/controller-utils/pkg/logging"
+	lc "github.com/gardener/landscaper/controller-utils/pkg/logging/constants"
 	deployerlib "github.com/gardener/landscaper/pkg/deployer/lib"
+	"github.com/gardener/landscaper/pkg/deployer/lib/targetselector"
 	lsutil "github.com/gardener/landscaper/pkg/utils"
 	"github.com/gardener/landscaper/pkg/utils/lock"
 	"github.com/gardener/landscaper/pkg/utils/read_write_layer"
@@ -65,15 +68,15 @@ func NewPodReconciler(
 	}
 }
 
-func (r *PodReconciler) Reconcile(ctx context.Context, req reconcile.Request) (reconcile.Result, error) {
-	logger, ctx := r.log.StartReconcileAndAddToContext(ctx, req)
+func (p *PodReconciler) Reconcile(ctx context.Context, req reconcile.Request) (reconcile.Result, error) {
+	logger, ctx := p.log.StartReconcileAndAddToContext(ctx, req)
 
 	if req.Namespace != lsutil.GetCurrentPodNamespace() {
 		return reconcile.Result{}, nil
 	}
 
 	podMetadata := lsutil.EmptyPodMetadata()
-	if err := r.lsClient.Get(ctx, req.NamespacedName, podMetadata); err != nil {
+	if err := p.lsClient.Get(ctx, req.NamespacedName, podMetadata); err != nil {
 		if apierrors.IsNotFound(err) {
 			logger.Debug(err.Error())
 			return reconcile.Result{}, nil
@@ -81,16 +84,27 @@ func (r *PodReconciler) Reconcile(ctx context.Context, req reconcile.Request) (r
 		return reconcile.Result{}, err
 	}
 
-	key, ok := r.getReconcileDeployItemRequest(podMetadata)
+	deployItemKey, ok := p.getDeployItemKey(podMetadata)
 	if !ok {
 		return reconcile.Result{}, nil
 	}
 
-	deployItem, err := getAndCheckReconcile(ctx, r.lsClient, r.config, key)
+	logger, ctx = logging.FromContextOrNew(ctx, nil, lc.KeyResource, deployItemKey.String())
+
+	deployItem := &lsv1alpha1.DeployItem{}
+	if err := read_write_layer.GetDeployItem(ctx, p.lsClient, deployItemKey, deployItem); err != nil {
+		if apierrors.IsNotFound(err) {
+			logger.Debug(err.Error())
+			return reconcile.Result{}, nil
+		}
+		return reconcile.Result{}, err
+	}
+
+	responsible, err := p.checkResponsibility(ctx, deployItem)
 	if err != nil {
 		return reconcile.Result{}, err
 	}
-	if deployItem == nil {
+	if !responsible {
 		return reconcile.Result{}, nil
 	}
 
@@ -98,7 +112,7 @@ func (r *PodReconciler) Reconcile(ctx context.Context, req reconcile.Request) (r
 		diMetaData := lsutil.EmptyDeployItemMetadata()
 		diMetaData.ObjectMeta = deployItem.ObjectMeta
 
-		locker := lock.NewLocker(r.lsClient, r.hostClient, r.callerName)
+		locker := lock.NewLocker(p.lsClient, p.hostClient, p.callerName)
 		syncObject, lsErr := locker.LockDI(ctx, diMetaData)
 		if lsErr != nil {
 			return lsutil.LogHelper{}.LogErrorAndGetReconcileResult(ctx, lsErr)
@@ -114,14 +128,13 @@ func (r *PodReconciler) Reconcile(ctx context.Context, req reconcile.Request) (r
 	}
 
 	lsv1alpha1helper.Touch(&deployItem.ObjectMeta)
-	if err = read_write_layer.NewWriter(r.lsClient).UpdateDeployItem(ctx, read_write_layer.W000030, deployItem); err != nil {
+	if err = read_write_layer.NewWriter(p.lsClient).UpdateDeployItem(ctx, read_write_layer.W000030, deployItem); err != nil {
 		return reconcile.Result{}, err
 	}
 	return reconcile.Result{}, nil
 }
 
-func (p *PodReconciler) getReconcileDeployItemRequest(object metav1.Object) (client.ObjectKey, bool) {
-
+func (p *PodReconciler) getDeployItemKey(object metav1.Object) (client.ObjectKey, bool) {
 	key := client.ObjectKey{}
 	ok := false
 
@@ -129,11 +142,41 @@ func (p *PodReconciler) getReconcileDeployItemRequest(object metav1.Object) (cli
 	if !ok {
 		return key, false
 	}
+
 	key.Namespace, ok = object.GetLabels()[container.ContainerDeployerDeployItemNamespaceLabel]
 	if !ok {
 		return key, false
 	}
+
 	return key, true
+}
+
+func (p *PodReconciler) checkResponsibility(ctx context.Context, deployItem *lsv1alpha1.DeployItem) (bool, error) {
+	logger, ctx := logging.FromContextOrNew(ctx, nil)
+
+	if deployItem.Spec.Type != Type {
+		logger.Debug("DeployItem is of wrong type", lc.KeyDeployItemType, deployItem.Spec.Type)
+		return false, nil
+	}
+
+	if deployItem.Spec.Target != nil {
+		target := &lsv1alpha1.Target{}
+		if err := p.lsClient.Get(ctx, deployItem.Spec.Target.NamespacedName(), target); err != nil {
+			return false, fmt.Errorf("unable to get target for deploy item: %w", err)
+		}
+		if len(p.config.TargetSelector) != 0 {
+			matched, err := targetselector.MatchOne(target, p.config.TargetSelector)
+			if err != nil {
+				return false, fmt.Errorf("unable to match target selector: %w", err)
+			}
+			if !matched {
+				logger.Debug("The deploy item's target does not match the given target selector")
+				return false, nil
+			}
+		}
+	}
+
+	return true, nil
 }
 
 type namespaceAndAnnotationPredicate struct {
