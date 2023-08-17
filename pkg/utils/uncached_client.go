4cb686700bcdbc334f1f7fd25e240c100d7fcdc4
Achim Weigel <achim.weigel@sap.com>
2023-08-17 08:46:20 +0200

    parallel controller tests

--- pkg/utils/uncached_client.go
+++ pkg/utils/uncached_client.go
@@ -1,11 +1,30 @@
 package utils
 
 import (
+	"fmt"
+	"reflect"
+	"strconv"
+
 	"k8s.io/client-go/rest"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/gardener/landscaper/controller-utils/pkg/logging"
 )
 
 func NewUncachedClient(config *rest.Config, options client.Options) (client.Client, error) {
+	log, err := logging.GetLogger()
+
+	if config.RateLimiter != nil {
+		log.Info("NewUncachedClient-RateLimiter: " + reflect.TypeOf(config.RateLimiter).String())
+	}
+
+	log.Info("NewUncachedClient-OldBurst: " + strconv.Itoa(config.Burst))
+	log.Info("NewUncachedClient-OldQPS: " + fmt.Sprintf("%v", config.QPS))
+
+	config.RateLimiter = nil
+	config.Burst = 1000
+	config.QPS = 1000
+
 	options.Cache = nil
 
 	c, err := client.New(config, options)
