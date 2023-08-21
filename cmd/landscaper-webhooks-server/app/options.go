891ecae1f017a9e54cd904aae6c4f5251a097296
Johannes Aubart <johannes.aubart@sap.com>
2023-08-21 09:09:09 +0200

    adapt landscaper webhook implementation to new webhook library (run-int-tests) (#803)

--- cmd/landscaper-webhooks-server/app/options.go
+++ cmd/landscaper-webhooks-server/app/options.go
@@ -6,79 +6,84 @@ package app
 
 import (
 	goflag "flag"
-	"strings"
 
 	"github.com/gardener/landscaper/apis/core"
 
 	flag "github.com/spf13/pflag"
-	"k8s.io/apimachinery/pkg/util/validation/field"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/gardener/landscaper/controller-utils/pkg/logging"
+	webhooklib "github.com/gardener/landscaper/controller-utils/pkg/webhook"
 	webhook "github.com/gardener/landscaper/pkg/utils/webhook"
 )
 
-// constant
-func defaultWebhookedResources() map[string]webhook.WebhookedResourceDefinition {
-	return map[string]webhook.WebhookedResourceDefinition{
-		"installations": {
-			APIGroup:     core.GroupName,
-			APIVersions:  []string{"v1alpha1"},
-			ResourceName: "installations",
+var landscaperSkipValidationSelector = &metav1.LabelSelector{
+	MatchExpressions: []metav1.LabelSelectorRequirement{
+		{
+			Operator: metav1.LabelSelectorOpNotIn,
+			Key:      "validation.landscaper.gardener.cloud/skip-validation",
+			Values:   []string{"true"},
 		},
-		"deployitems": {
-			APIGroup:     core.GroupName,
-			APIVersions:  []string{"v1alpha1"},
-			ResourceName: "deployitems",
-		},
-		"executions": {
-			APIGroup:     core.GroupName,
-			APIVersions:  []string{"v1alpha1"},
-			ResourceName: "executions",
-		},
-		"targets": {
-			APIGroup:     core.GroupName,
-			APIVersions:  []string{"v1alpha1"},
-			ResourceName: "targets",
-		},
-	}
+	},
 }
+
+var defaultWebhooks = webhooklib.NewWebhookRegistry().
+	Register(&webhooklib.Webhook{
+		Name:          "installations",
+		Type:          webhooklib.ValidatingWebhook,
+		APIGroup:      core.GroupName,
+		APIVersions:   []string{"v1alpha1"},
+		ResourceName:  "installations",
+		Operations:    webhooklib.Operations(webhooklib.CREATE, webhooklib.UPDATE),
+		LabelSelector: landscaperSkipValidationSelector,
+		Process:       webhook.InstallationWebhookLogic,
+	}).
+	Register(&webhooklib.Webhook{
+		Name:          "deployitems",
+		Type:          webhooklib.ValidatingWebhook,
+		APIGroup:      core.GroupName,
+		APIVersions:   []string{"v1alpha1"},
+		ResourceName:  "deployitems",
+		Operations:    webhooklib.Operations(webhooklib.CREATE, webhooklib.UPDATE),
+		LabelSelector: landscaperSkipValidationSelector,
+		Process:       webhook.DeployItemWebhookLogic,
+	}).
+	Register(&webhooklib.Webhook{
+		Name:          "executions",
+		Type:          webhooklib.ValidatingWebhook,
+		APIGroup:      core.GroupName,
+		APIVersions:   []string{"v1alpha1"},
+		ResourceName:  "executions",
+		Operations:    webhooklib.Operations(webhooklib.CREATE, webhooklib.UPDATE),
+		LabelSelector: landscaperSkipValidationSelector,
+		Process:       webhook.ExecutionWebhookLogic,
+	}).
+	Register(&webhooklib.Webhook{
+		Name:          "targets",
+		Type:          webhooklib.ValidatingWebhook,
+		APIGroup:      core.GroupName,
+		APIVersions:   []string{"v1alpha1"},
+		ResourceName:  "targets",
+		Operations:    webhooklib.Operations(webhooklib.CREATE, webhooklib.UPDATE),
+		LabelSelector: landscaperSkipValidationSelector,
+		Process:       webhook.TargetWebhookLogic,
+	})
 
 type options struct {
-	log                         logging.Logger
-	port                        int    // port where the webhook server is running
-	disabledWebhooks            string // lists disabled webhooks as a comma-separated string
-	webhookServiceNamespaceName string // webhook service namespace and name in the format <namespace>/<name>
-	webhookServicePort          int32  // port of the webhook service
-	webhookURL                  string // URL referring to the webhook service running externally
-	certificatesNamespace       string // the namespace in which the webhook credentials are being created/updated
-
-	webhook webhookOptions
-}
-
-// options for the webhook (generated from raw CLI options for easier usage)
-type webhookOptions struct {
-	webhookServiceNamespace string                                // webhook service namespace
-	webhookServiceName      string                                // webhook service name
-	webhookServicePort      int32                                 // port of the webhook service
-	certificatesNamespace   string                                // the certificate namespace
-	enabledWebhooks         []webhook.WebhookedResourceDefinition // which resources should be watched by the webhook
+	log           logging.Logger
+	webhookConfig *webhooklib.WebhookFlags
 }
 
 func NewOptions() *options {
 	return &options{
-		webhook: webhookOptions{},
+		webhookConfig: webhooklib.NewWebhookFlags(),
 	}
 }
 
 func (o *options) AddFlags(fs *flag.FlagSet) {
-	fs.IntVar(&o.port, "port", 9443, "Specify the port of the webhook server")
-	fs.StringVar(&o.disabledWebhooks, "disable-webhooks", "", "Specify validation webhooks that should be disabled ('all' to disable validation completely)")
-	fs.StringVar(&o.webhookServiceNamespaceName, "webhook-service", "", "Specify namespace and name of the webhook service (format: <namespace>/<name>)")
-	fs.Int32Var(&o.webhookServicePort, "webhook-service-port", 9443, "Specify the port of the webhook service")
-	fs.StringVar(&o.webhookURL, "webhook-url", "", "Specify the URL of the external webhook service (scheme://host:port")
-	fs.StringVar(&o.certificatesNamespace, "certificates-namespace", "", "Specify the namespace in which the certificates are being stored")
+	o.webhookConfig.AddFlags(fs)
 	logging.InitFlags(fs)
-
 	flag.CommandLine.AddGoFlagSet(goflag.CommandLine)
 }
 
@@ -90,108 +95,10 @@ func (o *options) Complete() error {
 	}
 	o.log = log
 
-	err = o.validate() // validate options
+	err = o.webhookConfig.Complete(defaultWebhooks)
 	if err != nil {
 		return err
 	}
 
-	allErrs := field.ErrorList{}
-	o.webhook.webhookServicePort = o.webhookServicePort
-	o.webhook.enabledWebhooks = filterWebhookedResources(defaultWebhookedResources(), stringListToMap(o.disabledWebhooks))
-	if len(o.webhook.enabledWebhooks) != 0 && len(o.webhookServiceNamespaceName) != 0 {
-		webhookService := strings.Split(o.webhookServiceNamespaceName, "/")
-		o.webhook.webhookServiceNamespace = webhookService[0]
-		o.webhook.webhookServiceName = webhookService[1]
-	}
-	o.webhook.certificatesNamespace = getCertificateNamespace(o)
-	return allErrs.ToAggregate()
-}
-
-// validates the options
-func (o *options) validate() error {
-	allErrs := field.ErrorList{}
-	dwr := defaultWebhookedResources()
-	if len(o.disabledWebhooks) != 0 { // something has been disabled
-		// validate that no unknown values are in the list of to-be-disabled webhooks
-		allowedWebhooks := allowedWebhookDisables()
-		disabledWebhooks := strings.Split(o.disabledWebhooks, ",")
-		for _, elem := range disabledWebhooks {
-			if _, ok := dwr[elem]; (elem != "all") && !ok {
-				allErrs = append(allErrs, field.NotSupported(field.NewPath("--disable-webhooks"), elem, allowedWebhooks))
-			}
-		}
-	}
-
-	// webhookServiceNamespaceName/webhookServicePort and webhookURL are mutually exclusive
-	if len(o.webhookURL) > 0 && len(o.webhookServiceNamespaceName) > 0 {
-		allErrs = append(allErrs, field.Invalid(field.NewPath("--webhook-url"), o.webhookURL, "must not specified together with --webhook-service"))
-	}
-
-	// validate service name and namespace
-	if len(o.webhookServiceNamespaceName) > 0 {
-		ws := strings.Split(o.webhookServiceNamespaceName, "/")
-		if len(ws) < 2 {
-			allErrs = append(allErrs, field.Invalid(field.NewPath("--webhook-service"), o.webhookServiceNamespaceName, "must have the format '<namespace>/<name>'"))
-		} else {
-			if len(ws[0]) == 0 || len(ws[1]) == 0 {
-				allErrs = append(allErrs, field.Invalid(field.NewPath("--webhook-service"), o.webhookServiceNamespaceName, "neither name nor namespace of the webhook service must be empty"))
-			}
-		}
-	}
-	// validate ports
-	if o.port <= 0 {
-		allErrs = append(allErrs, field.Invalid(field.NewPath("--port"), o.port, "must be greater than zero"))
-	}
-	if o.webhookServicePort <= 0 && len(o.webhookURL) == 0 {
-		allErrs = append(allErrs, field.Invalid(field.NewPath("--webhook-service-port"), o.webhookServicePort, "must be greater than zero"))
-	}
-	return allErrs.ToAggregate()
-}
-
-// filterWebhookedResources returns a slice of WebhookedResourceDefinitions that contains only those of the given webhookedResources whose ResourceName is not specified in disabledWebhooks
-func filterWebhookedResources(webhookedResources map[string]webhook.WebhookedResourceDefinition, disabledWebhooks map[string]bool) []webhook.WebhookedResourceDefinition {
-	fwr := []webhook.WebhookedResourceDefinition{}
-	if _, ok := disabledWebhooks["all"]; ok {
-		return fwr // all webhooks disabled, return empty slice
-	}
-	for _, wr := range webhookedResources {
-		if _, ok := disabledWebhooks[wr.ResourceName]; !ok {
-			fwr = append(fwr, wr)
-		}
-	}
-	return fwr
-}
-
-// allowedWebhookDisables computes a list of allowed values for the '--disable-webhooks' option
-func allowedWebhookDisables() []string {
-	dwr := defaultWebhookedResources()
-	res := make([]string, len(dwr)+1)
-	c := 0
-	for _, elem := range dwr {
-		res[c] = elem.ResourceName
-		c++
-	}
-	res[c] = "all"
-	return res
-}
-
-// stringListToMap turns a comma-separated list of strings into pseudo-set that maps all elements of the list to true
-func stringListToMap(opt string) map[string]bool {
-	res := map[string]bool{}
-	tmp := strings.Split(opt, ",")
-	for _, t := range tmp {
-		res[t] = true
-	}
-	return res
-}
-
-func getCertificateNamespace(opt *options) string {
-	if len(opt.certificatesNamespace) != 0 {
-		return opt.certificatesNamespace
-	}
-	if len(opt.webhookURL) != 0 {
-		return ""
-	} else {
-		return opt.webhook.webhookServiceNamespace
-	}
+	return nil
 }
--- pkg/utils/webhook/webhook.go
+++ pkg/utils/webhook/webhook.go
@@ -8,98 +8,25 @@ import (
 	"context"
 	"fmt"
 	"net/http"
-	"time"
 
 	lc "github.com/gardener/landscaper/controller-utils/pkg/logging/constants"
 
 	admissionv1 "k8s.io/api/admission/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/runtime/serializer"
 	"k8s.io/apimachinery/pkg/util/validation/field"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 
 	lscore "github.com/gardener/landscaper/apis/core"
 	"github.com/gardener/landscaper/apis/core/validation"
 	"github.com/gardener/landscaper/controller-utils/pkg/logging"
+	webhooklib "github.com/gardener/landscaper/controller-utils/pkg/webhook"
 )
-
-// ValidatorFromResourceType is a helper method that gets a resource type and returns the fitting validator
-func ValidatorFromResourceType(log logging.Logger, kubeClient client.Client, scheme *runtime.Scheme, resource string) (GenericValidator, error) {
-	abstrVal := newAbstractedValidator(log, kubeClient, scheme)
-	var val GenericValidator
-	switch resource {
-	case "installations":
-		val = &InstallationValidator{abstrVal}
-	case "deployitems":
-		val = &DeployItemValidator{abstrVal}
-	case "executions":
-		val = &ExecutionValidator{abstrVal}
-	case "targets":
-		val = &TargetValidator{abstrVal}
-	default:
-		return nil, fmt.Errorf("unable to find validator for resource type %q", resource)
-	}
-	return val, nil
-}
-
-type abstractValidator struct {
-	Client  client.Client
-	decoder runtime.Decoder
-	log     logging.Logger
-}
-
-// newAbstractedValidator creates a new abstracted validator
-func newAbstractedValidator(log logging.Logger, kubeClient client.Client, scheme *runtime.Scheme) abstractValidator {
-	return abstractValidator{
-		Client:  kubeClient,
-		decoder: serializer.NewCodecFactory(scheme).UniversalDecoder(),
-		log:     log,
-	}
-}
-
-// GenericValidator is an abstraction interface that implements admission.Handler and contains additional setter functions for the fields
-type GenericValidator interface {
-	Handle(context.Context, admission.Request) admission.Response
-}
 
 // INSTALLATION
 
-// InstallationValidator represents a validator for an Installation
-type InstallationValidator struct{ abstractValidator }
-
-// Handle handles a request to the webhook
-func (iv *InstallationValidator) Handle(ctx context.Context, req admission.Request) admission.Response {
-	logger := iv.log.WithValues(lc.KeyResourceGroup, req.Kind.Group, lc.KeyResourceKind, req.Kind.Kind, lc.KeyResourceVersion, req.Kind.Version)
-	ctx = logging.NewContext(ctx, logger)
-
-	timeBefore := time.Now()
-	result := iv.handlePrivate(ctx, req)
-
-	logIfDurationExceeded(ctx, timeBefore)
-
-	return result
-}
-
-func logIfDurationExceeded(ctx context.Context, timeBefore time.Time) {
-	logger, _ := logging.FromContextOrNew(ctx, []interface{}{lc.KeyMethod, "logIfDurationExceeded"})
-
-	timeAfter := time.Now()
-	diff := timeAfter.Sub(timeBefore)
-	// check if request requires more than 9 seconds because 10 seconds timeout is configured at the landscaper webhook
-	// calling this validation logic.
-	if diff > 9*time.Second {
-		logger.Info(fmt.Sprintf("validation request required more than 9 seconds: %s", diff.String()))
-	}
-}
-
-func (iv *InstallationValidator) handlePrivate(ctx context.Context, req admission.Request) admission.Response {
-	logger, _ := logging.FromContextOrNew(ctx, []interface{}{lc.KeyMethod, "InstallationValidator.handlePrivate"})
-
-	logger.Debug("Received request")
-
+var InstallationWebhookLogic webhooklib.WebhookLogic = func(ctx context.Context, req admission.Request, dec runtime.Decoder) admission.Response {
 	inst := &lscore.Installation{}
-	if _, _, err := iv.decoder.Decode(req.Object.Raw, nil, inst); err != nil {
+	if _, _, err := dec.Decode(req.Object.Raw, nil, inst); err != nil {
 		return admission.Errored(http.StatusBadRequest, err)
 	}
 
@@ -112,29 +39,11 @@ func (iv *InstallationValidator) handlePrivate(ctx context.Context, req admissio
 
 // DEPLOYITEM
 
-// DeployItemValidator represents a validator for a DeployItem
-type DeployItemValidator struct{ abstractValidator }
-
-// Handle handles a request to the webhook
-func (div *DeployItemValidator) Handle(ctx context.Context, req admission.Request) admission.Response {
-	logger := div.log.WithValues(lc.KeyResourceGroup, req.Kind.Group, lc.KeyResourceKind, req.Kind.Kind, lc.KeyResourceVersion, req.Kind.Version)
-	ctx = logging.NewContext(ctx, logger)
-
-	timeBefore := time.Now()
-	result := div.handlePrivate(ctx, req)
-
-	logIfDurationExceeded(ctx, timeBefore)
-
-	return result
-}
-
-func (div *DeployItemValidator) handlePrivate(ctx context.Context, req admission.Request) admission.Response {
-	logger, _ := logging.FromContextOrNew(ctx, []interface{}{lc.KeyMethod, "DeployItemValidator.handlePrivate"})
-
-	logger.Debug("Received request")
+var DeployItemWebhookLogic webhooklib.WebhookLogic = func(ctx context.Context, req admission.Request, dec runtime.Decoder) admission.Response {
+	logger, _ := logging.FromContextOrNew(ctx, []interface{}{lc.KeyMethod, "DeployItemWebhookLogic"})
 
 	di := &lscore.DeployItem{}
-	if _, _, err := div.decoder.Decode(req.Object.Raw, nil, di); err != nil {
+	if _, _, err := dec.Decode(req.Object.Raw, nil, di); err != nil {
 		return admission.Errored(http.StatusBadRequest, err)
 	}
 
@@ -145,7 +54,7 @@ func (div *DeployItemValidator) handlePrivate(ctx context.Context, req admission
 	// check if the type was updated for update events
 	if req.Operation == admissionv1.Update {
 		oldDi := &lscore.DeployItem{}
-		if _, _, err := div.decoder.Decode(req.OldObject.Raw, nil, oldDi); err != nil {
+		if _, _, err := dec.Decode(req.OldObject.Raw, nil, oldDi); err != nil {
 			return admission.Errored(http.StatusBadRequest, err)
 		}
 		if oldDi.Spec.Type != di.Spec.Type {
@@ -159,29 +68,9 @@ func (div *DeployItemValidator) handlePrivate(ctx context.Context, req admission
 
 // EXECUTION
 
-// ExecutionValidator represents a validator for an Execution
-type ExecutionValidator struct{ abstractValidator }
-
-// Handle handles a request to the webhook
-func (ev *ExecutionValidator) Handle(ctx context.Context, req admission.Request) admission.Response {
-	logger := ev.log.WithValues(lc.KeyResourceGroup, req.Kind.Group, lc.KeyResourceKind, req.Kind.Kind, lc.KeyResourceVersion, req.Kind.Version)
-	ctx = logging.NewContext(ctx, logger)
-
-	timeBefore := time.Now()
-	result := ev.handlePrivate(ctx, req)
-
-	logIfDurationExceeded(ctx, timeBefore)
-
-	return result
-}
-
-func (ev *ExecutionValidator) handlePrivate(ctx context.Context, req admission.Request) admission.Response {
-	logger, _ := logging.FromContextOrNew(ctx, []interface{}{lc.KeyMethod, "ExecutionValidator.handlePrivate"})
-
-	logger.Debug("Received request")
-
+var ExecutionWebhookLogic webhooklib.WebhookLogic = func(ctx context.Context, req admission.Request, dec runtime.Decoder) admission.Response {
 	exec := &lscore.Execution{}
-	if _, _, err := ev.decoder.Decode(req.Object.Raw, nil, exec); err != nil {
+	if _, _, err := dec.Decode(req.Object.Raw, nil, exec); err != nil {
 		return admission.Errored(http.StatusBadRequest, err)
 	}
 
@@ -194,29 +83,9 @@ func (ev *ExecutionValidator) handlePrivate(ctx context.Context, req admission.R
 
 // TARGET
 
-// TargetValidator represents a validator for a Target
-type TargetValidator struct{ abstractValidator }
-
-// Handle handles a request to the webhook
-func (tv *TargetValidator) Handle(ctx context.Context, req admission.Request) admission.Response {
-	logger := tv.log.WithValues(lc.KeyResourceGroup, req.Kind.Group, lc.KeyResourceKind, req.Kind.Kind, lc.KeyResourceVersion, req.Kind.Version)
-	ctx = logging.NewContext(ctx, logger)
-
-	timeBefore := time.Now()
-	result := tv.handlePrivate(ctx, req)
-
-	logIfDurationExceeded(ctx, timeBefore)
-
-	return result
-}
-
-func (tv *TargetValidator) handlePrivate(ctx context.Context, req admission.Request) admission.Response {
-	logger, _ := logging.FromContextOrNew(ctx, []interface{}{lc.KeyMethod, "TargetValidator.handlePrivate"})
-
-	logger.Debug("Received request")
-
+var TargetWebhookLogic webhooklib.WebhookLogic = func(ctx context.Context, req admission.Request, dec runtime.Decoder) admission.Response {
 	t := &lscore.Target{}
-	if _, _, err := tv.decoder.Decode(req.Object.Raw, nil, t); err != nil {
+	if _, _, err := dec.Decode(req.Object.Raw, nil, t); err != nil {
 		return admission.Errored(http.StatusBadRequest, err)
 	}
 
