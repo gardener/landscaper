07a1a2bea34126a823e3bfcd8a8cc661c084c534
Fabian Burth <fabian.burth@sap.com>
2023-11-08 13:24:25 +0100

    adjust scripts to decide whether to push or not to push

--- docs/guided-tour/blueprints/helm-chart-resource/commands/upload-component-version.sh
+++ docs/guided-tour/blueprints/helm-chart-resource/commands/upload-component-version.sh
@@ -14,9 +14,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# OCM Repository the component version is pushed to
+OCM_REPOSITORY="eu.gcr.io/gardener-project/landscaper/examples"
+
 # Currently either "v2" or "ocm.software/v3alpha1" 
 SCHEMA_VERSION=$1
 LOCAL_BLUEPRINT=$2
+PUSH=$3
 
 PATH_SUFFIX=""
 if [[ $SCHEMA_VERSION == "" ]]; then
@@ -32,7 +36,8 @@ else
 fi
 
 if [[ $LOCAL_BLUEPRINT == "" ]]; then
-    echo "Please specify whether the resource shall be added as reference (external) or as local blob (local) as argument 2"
+    echo "Please specify whether the resource shall be added as reference (external) or as local blob (local) as " \
+        "argument 2"
     exit 1
 elif [[ $LOCAL_BLUEPRINT == "local" ]] || [[ $LOCAL_BLUEPRINT == "external" ]]; then
     PATH_SUFFIX="${PATH_SUFFIX}-${LOCAL_BLUEPRINT}"
@@ -41,6 +46,14 @@ else
     exit 1
 fi
     
+if [[ $PUSH == "" ]]; then
+    echo "Please specify whether the component version shall be pushed to ${OCM_REPOSITORY} (push) or whether this " \
+        "step shall be skipped (no-push) as argument 3."
+    exit 1
+elif [[ $PUSH != "no-push" ]] && [[ $PUSH != "push" ]]; then
+    echo "invalid argument value ${NO_PUSH}, please specify either push or no-push"
+    exit 1
+fi
 
 SCRIPT_DIR=$(dirname "$0")
 PARENT_DIR="${SCRIPT_DIR}/.."
@@ -54,29 +67,41 @@ if [ -d "${COMPONENT_DIR}" ]; then
 fi
 
 COMPONENT_NAME="github.com/gardener/landscaper-examples/guided-tour/helm-chart-resource"
-# version 2.x.x because 1.x.x are reserved for the legacy components created with component-cli and the corresponding examples
+# version 2.x.x because 1.x.x are reserved for the legacy components created with component-cli and the corresponding 
+# examples
 COMPONENT_VERSION="2.0.0"
 COMPONENT_PROVIDER="internal"
 
-# A Component Archive is a file system representation of a OCM Repository capable of hosting exactly one Component Version
+# A Component Archive is a file system representation of a OCM Repository capable of hosting exactly one Component 
+# Version
 echo "creating component archive at ${COMPONENT_DIR}"
-ocm create componentarchive ${COMPONENT_NAME} ${COMPONENT_VERSION} --provider ${COMPONENT_PROVIDER} --file ${COMPONENT_DIR} --scheme ${SCHEMA_VERSION}
+ocm create componentarchive ${COMPONENT_NAME} ${COMPONENT_VERSION} --provider ${COMPONENT_PROVIDER} \
+    --file ${COMPONENT_DIR} --scheme ${SCHEMA_VERSION}
 
 # Add the blueprint as a resource to the component version
 if [[ $LOCAL_BLUEPRINT == "local" ]]; then
     # Add blueprint as local (also known as inline) resource
-    ocm add resource ${COMPONENT_DIR} --type blueprint --name blueprint --version 1.0.0 --inputType dir --inputPath "${BLUEPRINT_DIR}" --inputCompress --mediaType "application/vnd.gardener.landscaper.blueprint.v1+tar+gzip"
+    ocm add resource ${COMPONENT_DIR} --type blueprint --name blueprint --version 1.0.0 --inputType dir \
+        --inputPath "${BLUEPRINT_DIR}" --inputCompress \
+        --mediaType "application/vnd.gardener.landscaper.blueprint.v1+tar+gzip"
 elif [[  $LOCAL_BLUEPRINT == "external" ]]; then
     # or, if the blueprint is already uploaded to an oci registry, e.g. with the landscaper-cli 
     # Add the image reference to the blueprint
-    ocm add resource ${COMPONENT_DIR} --type blueprint --name blueprint --version 1.0.0 --accessType ociArtifact --reference eu.gcr.io/gardener-project/landscaper/examples/blueprints/guided-tour/helm-chart-resource:1.0.0
+    ocm add resource ${COMPONENT_DIR} --type blueprint --name blueprint --version 1.0.0 --accessType ociArtifact \
+        --reference eu.gcr.io/gardener-project/landscaper/examples/blueprints/guided-tour/helm-chart-resource:1.0.0
 fi
 
 # Add the helm chart as a resource to the component version
 # Adding resources besides the blueprint as local blob is currently not supported by the landscaper
-ocm add resource ${COMPONENT_DIR} --type helmChart --name hello-world-chart --version 1.0.0 --accessType ociArtifact --reference eu.gcr.io/gardener-project/landscaper/examples/charts/hello-world:1.0.0
+ocm add resource ${COMPONENT_DIR} --type helmChart --name hello-world-chart --version 1.0.0 --accessType ociArtifact \
+    --reference eu.gcr.io/gardener-project/landscaper/examples/charts/hello-world:1.0.0
 
-# Transfer the Component Version from the file system representation of an OCM Repository to an oci registry representation of an OCM Repository
-# echo "pushing component version to oci registry"
-OCM_REPOSITORY="eu.gcr.io/gardener-project/landscaper/examples"
-ocm transfer componentarchive --overwrite ${COMPONENT_DIR} ${OCM_REPOSITORY}
+# Transfer the Component Version from the file system representation of an OCM Repository to an oci registry 
+# representation of an OCM Repository echo "pushing component version to oci registry"
+if [[ $PUSH == "no-push" ]]; then
+    echo "pushig to ${OCM_REPOSITORY} is skipped"
+elif [[ $PUSH == "push" ]]; then
+    # Transfer the Component Version from the file system representation of an OCM Repository to an oci registry 
+    # representation of an OCM Repository echo "pushing component version to oci registry"
+    ocm transfer componentarchive --overwrite ${COMPONENT_DIR} ${OCM_REPOSITORY}
+fi
--- docs/guided-tour/templating/components/commands/upload-component-version-core.sh
+++ docs/guided-tour/templating/components/commands/upload-component-version-core.sh
@@ -14,8 +14,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# OCM Repository the component version is pushed to
+OCM_REPOSITORY="eu.gcr.io/gardener-project/landscaper/examples"
+
 # Currently either "v2" or "ocm.software/v3alpha1" 
 SCHEMA_VERSION=$1
+PUSH=$2
 
 PATH_SUFFIX=""
 if [[ $SCHEMA_VERSION == "" ]]; then
@@ -30,6 +34,15 @@ else
     exit 1
 fi
 
+if [[ $PUSH == "" ]]; then
+    echo "Please specify whether the component version shall be pushed to ${OCM_REPOSITORY} (push) or whether this " \
+        "step shall be skipped (no-push) as argument 3."
+    exit 1
+elif [[ $PUSH != "no-push" ]] && [[ $PUSH != "push" ]]; then
+    echo "invalid argument value ${NO_PUSH}, please specify either push or no-push"
+    exit 1
+fi
+
 SCRIPT_DIR=$(dirname "$0")
 PARENT_DIR="${SCRIPT_DIR}/.."
 COMPONENT_DIR="$(realpath "${PARENT_DIR}")/component-core/${PATH_SUFFIX}"
@@ -41,25 +54,39 @@ if [ -d "${COMPONENT_DIR}" ]; then
 fi
 
 COMPONENT_NAME="github.com/gardener/landscaper-examples/guided-tour/templating-components-core"
-# version 2.x.x because 1.x.x are reserved for the legacy components created with component-cli and the corresponding examples
+# version 2.x.x because 1.x.x are reserved for the legacy components created with component-cli and the corresponding 
+# examples
 COMPONENT_VERSION="2.0.0"
 COMPONENT_PROVIDER="internal"
 
-# A Component Archive is a file system representation of a OCM Repository capable of hosting exactly one Component Version
+# A Component Archive is a file system representation of a OCM Repository capable of hosting exactly one Component 
+# Version
 echo "creating component archive at ${COMPONENT_DIR}"
-ocm create componentarchive ${COMPONENT_NAME} ${COMPONENT_VERSION} --provider ${COMPONENT_PROVIDER} --file ${COMPONENT_DIR} --scheme ${SCHEMA_VERSION}
+ocm create componentarchive ${COMPONENT_NAME} ${COMPONENT_VERSION} --provider ${COMPONENT_PROVIDER} \
+    --file ${COMPONENT_DIR} --scheme ${SCHEMA_VERSION}
 
 # Add an oci image as an external resource to the component version
 # Adding resources besides the blueprint as local blob is currently not supported by the landscaper
-# --skip-digest-generation since the images referenced here do not actually exist and are merely added for templating examples
-ocm add resource ${COMPONENT_DIR} --skip-digest-generation --type ociImage --name image-a --version 1.0.0 --accessType ociArtifact --reference eu.gcr.io/gardener-project/landscaper/examples/images/image-a:1.0.0 --label landscaper.gardener.cloud/guided-tour/type='type-a'
+# --skip-digest-generation since the images referenced here do not actually exist and are merely added for templating 
+# examples
+ocm add resource ${COMPONENT_DIR} --skip-digest-generation --type ociImage --name image-a --version 1.0.0 \
+    --accessType ociArtifact --reference eu.gcr.io/gardener-project/landscaper/examples/images/image-a:1.0.0 \
+    --label landscaper.gardener.cloud/guided-tour/type='type-a'
 
 # Add another oci image as an external resource to the component version
 # Adding resources besides the blueprint as local blob is currently not supported by the landscaper
-# --skip-digest-generation since the images referenced here do not actually exist and are merely added for templating examples
-ocm add resource ${COMPONENT_DIR} --skip-digest-generation --type ociImage --name image-b --version 1.0.0 --accessType ociArtifact --reference eu.gcr.io/gardener-project/landscaper/examples/images/image-b:1.0.0 --label landscaper.gardener.cloud/guided-tour/auxiliary='aux-b'
+# --skip-digest-generation since the images referenced here do not actually exist and are merely added for templating 
+# examples
+ocm add resource ${COMPONENT_DIR} --skip-digest-generation --type ociImage --name image-b --version 1.0.0 \
+    --accessType ociArtifact --reference eu.gcr.io/gardener-project/landscaper/examples/images/image-b:1.0.0 \
+    --label landscaper.gardener.cloud/guided-tour/auxiliary='aux-b'
 
-# Transfer the Component Version from the file system representation of an OCM Repository to an oci registry representation of an OCM Repository
-# echo "pushing component version to oci registry"
-OCM_REPOSITORY="eu.gcr.io/gardener-project/landscaper/examples"
-ocm transfer componentarchive --overwrite ${COMPONENT_DIR} ${OCM_REPOSITORY}
+# Transfer the Component Version from the file system representation of an OCM Repository to an oci registry 
+# representation of an OCM Repository echo "pushing component version to oci registry"
+if [[ $PUSH == "no-push" ]]; then
+    echo "pushig to ${OCM_REPOSITORY} is skipped"
+elif [[ $PUSH == "push" ]]; then
+    # Transfer the Component Version from the file system representation of an OCM Repository to an oci registry 
+    # representation of an OCM Repository echo "pushing component version to oci registry"
+    ocm transfer componentarchive --overwrite ${COMPONENT_DIR} ${OCM_REPOSITORY}
+fi
