b9fed689e83a5b2fe6e86e5e6751ebfaa7e169cc
guewa <72857276+guewa@users.noreply.github.com>
2023-11-28 12:49:07 +0100

    Update install-requirements.sh

--- hack/install-requirements.sh
+++ hack/install-requirements.sh
@@ -4,19 +4,31 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-set -e
+set -e -x
 
 CURRENT_DIR=$(dirname $0)
 PROJECT_ROOT="${CURRENT_DIR}"/..
 
 # test OCI registry currently doesnt support darwin/arm64 / force amd64
 ARCH_ARG=$(go env GOARCH)
-if [[ $(go env GOOS) == "darwin" && $(go env GOARCH) == "arm64" ]]; then
+GO_OS=$(go env GOOS)
+if [[ ${GO_OS} == "darwin" && ${ARCH_ARG} == "arm64" ]]; then
   ARCH_ARG="amd64"
 fi
 
 mkdir -p ${PROJECT_ROOT}/tmp/test/registry
-curl -sfL "https://storage.googleapis.com/gardener-public/test/oci-registry/registry-$(go env GOOS)-${ARCH_ARG}" --output ${PROJECT_ROOT}/tmp/test/registry/registry
+REGISTRY_VERSION="2.8.3"
+REGISTRY_FILENAME="registry_${REGISTRY_VERSION}_${GO_OS}_${ARCH_ARG}.tar.gz"
+REGISTRY_FILEPATH="${PROJECT_ROOT}/tmp/test/${REGISTRY_FILENAME}"
+REGISTRY_URL="https://github.com/distribution/distribution/releases/download/v${REGISTRY_VERSION}/${REGISTRY_FILENAME}"
+echo "Downloading registry from \"${REGISTRY_URL}\" to \"${REGISTRY_FILEPATH}\""
+curl -sfLv "${REGISTRY_URL}" --output ${REGISTRY_FILEPATH}
+
+echo "Extracting file \"registry\" from \"${REGISTRY_FILEPATH}\" to \"${PROJECT_ROOT}/tmp/test/registry\""
+tar -C ${PROJECT_ROOT}/tmp/test/registry -xzf ${REGISTRY_FILEPATH} registry
+
+# old deleted location
+# curl -sfL "https://storage.googleapis.com/gardener-public/test/oci-registry/registry-$(go env GOOS)-${ARCH_ARG}" --output ${PROJECT_ROOT}/tmp/test/registry/registry
 chmod +x ${PROJECT_ROOT}/tmp/test/registry/registry
 
 GO111MODULE=off go get golang.org/x/tools/cmd/goimports
