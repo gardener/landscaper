a66f61e15353d35651102abdd8fbf094a3c7344b
René Schünemann <rene.schuenemann@sap.com>
2024-01-17 15:28:09 +0100

    simplify components definition

--- hack/generate-cd.sh
+++ hack/generate-cd.sh
@@ -23,75 +23,42 @@ PROVIDER=$1
 COMPONENT_ARCHIVE_PATH="$(mktemp -d)/ctf"
 COMMIT_SHA=$(git rev-parse HEAD)
 
-LANDSCAPER_COMPONENT_NAME="github.com/gardener/landscaper"
 LANDSCAPER_CHART_PATH="${SOURCE_PATH}/charts/landscaper"
 LANDSCAPER_CONTROLLER_RBAC_CHART_PATH="${SOURCE_PATH}/charts/landscaper/charts/rbac"
 LANDSCAPER_CONTROLLER_DEPLOYMENT_CHART_PATH="${SOURCE_PATH}/charts/landscaper/charts/landscaper"
 LANDSCAPER_AGENT_CHART_PATH="${SOURCE_PATH}/charts/landscaper-agent"
-LANDSCAPER_CONTROLLER_IMAGE_PATH="landscaper-controller"
-LANDSCAPER_WEBHOOKS_SERVER_IMAGE_PATH="landscaper-webhooks-server"
-LANDSCAPER_AGENT_IMAGE_PATH="landscaper-agent"
-
-HELM_DEPLOYER_COMPONENT_NAME="github.com/gardener/landscaper/helm-deployer"
 HELM_DEPLOYER_CHART_PATH="${SOURCE_PATH}/charts/helm-deployer"
-HELM_DEPLOYER_CONTROLLER_IMAGE_PATH="helm-deployer-controller"
-
-MANIFEST_DEPLOYER_COMPONENT_NAME="github.com/gardener/landscaper/manifest-deployer"
 MANIFEST_DEPLOYER_CHART_PATH="${SOURCE_PATH}/charts/manifest-deployer"
-MANIFEST_DEPLOYER_CONTROLLER_IMAGE_PATH="manifest-deployer-controller"
-
-CONTAINER_DEPLOYER_COMPONENT_NAME="github.com/gardener/landscaper/container-deployer"
 CONTAINER_DEPLOYER_CHART_PATH="${SOURCE_PATH}/charts/container-deployer"
-CONTAINER_DEPLOYER_CONTROLLER_IMAGE_PATH="container-deployer-controller"
-CONTAINER_DEPLOYER_INIT_IMAGE_PATH="container-deployer-init"
-CONTAINER_DEPLOYER_WAIT_IMAGE_PATH="container-deployer-wait"
-
-MOCK_DEPLOYER_COMPONENT_NAME="github.com/gardener/landscaper/mock-deployer"
 MOCK_DEPLOYER_CHART_PATH="${SOURCE_PATH}/charts/mock-deployer"
-MOCK_DEPLOYER_CONTROLLER_IMAGE_PATH="mock-deployer-controller"
 
 ocm add componentversions --create --file ${COMPONENT_ARCHIVE_PATH} ${SOURCE_PATH}/.landscaper/components.yaml \
   -- VERSION=${EFFECTIVE_VERSION} \
      COMMIT_SHA=${COMMIT_SHA} \
-     LANDSCAPER_COMPONENT_NAME=${LANDSCAPER_COMPONENT_NAME} \
-     HELM_DEPLOYER_COMPONENT_NAME=${HELM_DEPLOYER_COMPONENT_NAME} \
-     MANIFEST_DEPLOYER_COMPONENT_NAME=${MANIFEST_DEPLOYER_COMPONENT_NAME} \
-     MOCK_DEPLOYER_COMPONENT_NAME=${MOCK_DEPLOYER_COMPONENT_NAME} \
-     CONTAINER_DEPLOYER_COMPONENT_NAME=${CONTAINER_DEPLOYER_COMPONENT_NAME} \
-     COMPONENT_REPO=${LANDSCAPER_COMPONENT_NAME} \
      PROVIDER=${PROVIDER} \
-     LANDSCAPER_CONTROLLER_IMAGE_PATH=${LANDSCAPER_CONTROLLER_IMAGE_PATH} \
-     LANDSCAPER_WEBHOOKS_SERVER_IMAGE_PATH=${LANDSCAPER_WEBHOOKS_SERVER_IMAGE_PATH} \
-     LANDSCAPER_AGENT_IMAGE_PATH=${LANDSCAPER_AGENT_IMAGE_PATH} \
      LANDSCAPER_CHART_PATH=${LANDSCAPER_CHART_PATH} \
      LANDSCAPER_CONTROLLER_RBAC_CHART_PATH=${LANDSCAPER_CONTROLLER_RBAC_CHART_PATH} \
      LANDSCAPER_CONTROLLER_DEPLOYMENT_CHART_PATH=${LANDSCAPER_CONTROLLER_DEPLOYMENT_CHART_PATH} \
      LANDSCAPER_AGENT_CHART_PATH=${LANDSCAPER_AGENT_CHART_PATH} \
      HELM_DEPLOYER_CHART_PATH=${HELM_DEPLOYER_CHART_PATH} \
-     HELM_DEPLOYER_CONTROLLER_IMAGE_PATH=${HELM_DEPLOYER_CONTROLLER_IMAGE_PATH} \
      MANIFEST_DEPLOYER_CHART_PATH=${MANIFEST_DEPLOYER_CHART_PATH} \
-     MANIFEST_DEPLOYER_CONTROLLER_IMAGE_PATH=${MANIFEST_DEPLOYER_CONTROLLER_IMAGE_PATH} \
      CONTAINER_DEPLOYER_CHART_PATH=${CONTAINER_DEPLOYER_CHART_PATH} \
-     CONTAINER_DEPLOYER_CONTROLLER_IMAGE_PATH=${CONTAINER_DEPLOYER_CONTROLLER_IMAGE_PATH} \
-     CONTAINER_DEPLOYER_INIT_IMAGE_PATH=${CONTAINER_DEPLOYER_INIT_IMAGE_PATH} \
-     CONTAINER_DEPLOYER_WAIT_IMAGE_PATH=${CONTAINER_DEPLOYER_WAIT_IMAGE_PATH} \
      MOCK_DEPLOYER_CHART_PATH=${MOCK_DEPLOYER_CHART_PATH} \
-     MOCK_DEPLOYER_CONTROLLER_IMAGE_PATH=${MANIFEST_DEPLOYER_CONTROLLER_IMAGE_PATH}
 
 echo "> Transfer Component version ${EFFECTIVE_VERSION} to ${PROVIDER}"
 ocm transfer ctf --copy-resources --recursive --overwrite ${COMPONENT_ARCHIVE_PATH} ${PROVIDER}
 
 echo "> Remote Component Version Landscaper"
-ocm get componentversion --repo OCIRegistry::${PROVIDER} ${LANDSCAPER_COMPONENT_NAME}:${EFFECTIVE_VERSION} -o yaml
+ocm get componentversion --repo OCIRegistry::${PROVIDER} "github.com/gardener/landscaper:${EFFECTIVE_VERSION}" -o yaml
 
 echo "> Remote Component Version Helm Deployer"
-ocm get componentversion --repo OCIRegistry::${PROVIDER} ${HELM_DEPLOYER_COMPONENT_NAME}:${EFFECTIVE_VERSION} -o yaml
+ocm get componentversion --repo OCIRegistry::${PROVIDER} "github.com/gardener/landscaper/helm-deployer:${EFFECTIVE_VERSION}" -o yaml
 
 echo "> Remote Component Version Manifest Deployer"
-ocm get componentversion --repo OCIRegistry::${PROVIDER} ${MANIFEST_DEPLOYER_COMPONENT_NAME}:${EFFECTIVE_VERSION} -o yaml
+ocm get componentversion --repo OCIRegistry::${PROVIDER} "github.com/gardener/landscaper/manifest-deployer:${EFFECTIVE_VERSION}" -o yaml
 
 echo "> Remote Component Version Container Deployer"
-ocm get componentversion --repo OCIRegistry::${PROVIDER} ${CONTAINER_DEPLOYER_COMPONENT_NAME}:${EFFECTIVE_VERSION} -o yaml
+ocm get componentversion --repo OCIRegistry::${PROVIDER} "github.com/gardener/landscaper/container-deployer:${EFFECTIVE_VERSION}" -o yaml
 
 echo "> Remote Component Version Mock Deployer"
-ocm get componentversion --repo OCIRegistry::${PROVIDER} ${MOCK_DEPLOYER_COMPONENT_NAME}:${EFFECTIVE_VERSION} -o yaml
+ocm get componentversion --repo OCIRegistry::${PROVIDER} "github.com/gardener/landscaper/mock-deployer:${EFFECTIVE_VERSION}" -o yaml
