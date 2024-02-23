df46189bfc3471a4bb0d824286cd89702cc647da
enrico-kaack-comp <enrico.kaack@sap.com>
2024-02-23 17:33:00 +0100

    add context, target and useful extensions

--- .devcontainer/scripts/post-start.sh
+++ .devcontainer/scripts/post-start.sh
@@ -12,3 +12,10 @@ fi
 
 echo "Exporting minikube config to yaml file"
 kubectl config view --raw > ~/.kube/kubeconfig--minikube-local.yaml
+
+echo "Build minikube target and apply to cluster"
+# creates a target.landscaper.gardener.cloud by combining the target.yaml with an indended minikube kubeconfig. Uses <() as process substitution to use command output as a file for kubectl
+kubectl apply -f <(cat .devcontainer/target-template.yaml;  cat ~/.kube/kubeconfig--minikube-local.yaml | sed 's/^/      /')
+
+echo "Apply context to cluster"
+kubectl apply -f /workspaces/landscaper/.devcontainer/context.yaml
