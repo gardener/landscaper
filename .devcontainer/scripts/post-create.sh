f9985d59792aec0dafa3131f8e5ec6280e7eb347
enrico-kaack-comp <enrico.kaack@sap.com>
2024-02-28 19:30:16 +0100

    Fix fully working sample

--- .devcontainer/scripts/post-create.sh
+++ .devcontainer/scripts/post-create.sh
@@ -3,24 +3,22 @@
 set -e
 
 # run once on post-create and it will be always restarted automatically when the container restarts
-if [ $( curl https://localhost:443 )  ]; then #TODO improve detection of already running container
-  echo "docker registry already exist"
-else
-  echo "docker registry does not exist yet, start registry with ssl..."
-  
-  docker run -d -p 443:443 --restart always --name registry \
-  -v /registry/certs/:/certs \
-  -e REGISTRY_HTTP_ADDR=:443 \
-  -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/ociregistry.crt \
-  -e REGISTRY_HTTP_TLS_KEY=/certs/ociregistry.key \
-  registry:2
-fi
+echo "Start or create registry"
+docker start registry || docker run -d -p 443:443 --restart always --name registry \
+-v /registry/certs/:/certs \
+-e REGISTRY_HTTP_ADDR=:443 \
+-e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/ociregistry.crt \
+-e REGISTRY_HTTP_TLS_KEY=/certs/ociregistry.key \
+registry:2
 
+echo "Start minikube"
 minikube start
 
 # apply CRDs to minikube
+echo "Apply landscaper CRDs to minikube cluster"
 kubectl apply -f /workspaces/landscaper/.crd/
 
 # create sample ocm componentversion and add it to registry
+echo "Create and push a sample componentversion with blueprint"
 ocm add components --create --file /workspaces/landscaper/.devcontainer/sample-installation/sample-component /workspaces/landscaper/.devcontainer/sample-installation/components.yaml
 ocm transfer ctf /workspaces/landscaper/.devcontainer/sample-installation/sample-component OCIRegistry::localhost:443
