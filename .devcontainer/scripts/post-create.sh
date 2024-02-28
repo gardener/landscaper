51b3ff48c01de2ea63a116f202724d0fb1ebffb8
enrico-kaack-comp <enrico.kaack@sap.com>
2024-02-28 15:27:42 +0100

    add sample blueprint/cd into registry with ssl

--- .devcontainer/scripts/post-create.sh
+++ .devcontainer/scripts/post-create.sh
@@ -3,11 +3,17 @@
 set -e
 
 # run once on post-create and it will be always restarted automatically when the container restarts
-if [ $( docker ps -a -f name=registry | wc -l ) -eq 2 ]; then
+if [ $( curl https://localhost:443 )  ]; then #TODO improve detection of already running container
   echo "docker registry already exist"
 else
-  echo "docker registry does not exist yet, creating..."
-  docker run -d -p 5000:5000 --restart always --name registry registry:2
+  echo "docker registry does not exist yet, start registry with ssl..."
+  
+  docker run -d -p 443:443 --restart always --name registry \
+  -v /registry/certs/:/certs \
+  -e REGISTRY_HTTP_ADDR=:443 \
+  -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/ociregistry.crt \
+  -e REGISTRY_HTTP_TLS_KEY=/certs/ociregistry.key \
+  registry:2
 fi
 
 minikube start
@@ -15,3 +21,6 @@ minikube start
 # apply CRDs to minikube
 kubectl apply -f /workspaces/landscaper/.crd/
 
+# create sample ocm componentversion and add it to registry
+ocm add components --create --file /workspaces/landscaper/.devcontainer/sample-installation/sample-component /workspaces/landscaper/.devcontainer/sample-installation/components.yaml
+ocm transfer ctf /workspaces/landscaper/.devcontainer/sample-installation/sample-component OCIRegistry::localhost:443
