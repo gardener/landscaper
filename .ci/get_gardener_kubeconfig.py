f155f9f1649742e7f892d21c701a8434eae22001
René Schünemann <rene.schuenemann@sap.com>
2024-03-13 09:46:35 +0100

    fix gardener kubeconfig (run-int-tests)

--- .ci/get_gardener_kubeconfig.py
+++ .ci/get_gardener_kubeconfig.py
@@ -4,7 +4,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-import utils
+import tempfile
 import yaml
 
 from util import ctx
@@ -12,8 +12,8 @@ from util import ctx
 factory = ctx().cfg_factory()
 landscape_kubeconfig = factory.kubernetes("landscaper-integration-test")
 
-with utils.TempFileAuto(prefix="gardener_serviceaccount_kubeconfig_") as kubeconfig_temp_file:
+with tempfile.NamedTemporaryFile(mode="w+", prefix="gardener_serviceaccount_kubeconfig_", suffix=".yaml", delete=False) as kubeconfig_temp_file:
     kubeconfig_temp_file.write(yaml.safe_dump(landscape_kubeconfig.kubeconfig()))
-    landscape_kubeconfig_path = kubeconfig_temp_file.switch()
+    landscape_kubeconfig_path = kubeconfig_temp_file.name
 
     print(landscape_kubeconfig_path)
