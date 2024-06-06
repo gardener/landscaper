1d24109c60b9798b7c129031b1588b698d1fa5cf
Robert Gräff <robert.graeff@sap.com>
2024-06-06 09:52:51 +0200

    Guided tour: component references (#1130)
    * Guided tour: component references
    
    * add to tyble of content
    
    ---------
    
    Co-authored-by: Achim Weigel <achim.weigel@sap.com>

---
+++ docs/guided-tour/subinstallations/component-references/commands/delete-installation.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Gardener contributors
+#
+# SPDX-License-Identifier: Apache-2.0
+
+set -o errexit
+
+COMPONENT_DIR="$(dirname $0)/.."
+cd "${COMPONENT_DIR}"
+COMPONENT_DIR="$(pwd)"
+echo "COMPONENT_DIR: ${COMPONENT_DIR}"
+
+source "${COMPONENT_DIR}/commands/settings"
+
+echo "deleting installation"
+kubectl delete installation component-references -n "${NAMESPACE}" --kubeconfig="${RESOURCE_CLUSTER_KUBECONFIG_PATH}"
