6788cfb70d09c76222b34b34fd6a4b6676a163a1
achimweigel <achim.weigel@sap.com>
2024-07-29 15:54:07 +0200

    Add helm force option (#1189)
    * add helm force option (run-int-tests)
    
    * add helm force option (run-int-tests)

--- apis/deployer/helm/types_provider.go
+++ apis/deployer/helm/types_provider.go
@@ -158,6 +158,8 @@ type HelmDeploymentConfiguration struct {
 // HelmInstallConfiguration defines settings for a helm install operation.
 type HelmInstallConfiguration struct {
 	Atomic bool `json:"atomic,omitempty"`
+	Force  bool `json:"force,omitempty"`
+
 	// Timeout is the timeout for the operation in minutes.
 	// +optional
 	Timeout *lsv1alpha1.Duration `json:"timeout,omitempty"`
