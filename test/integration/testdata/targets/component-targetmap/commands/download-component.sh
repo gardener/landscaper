431001d05a7cd2842e958593b77370f12868015b
Robert Gräff <robert.graeff@sap.com>
2025-03-06 08:46:28 +0100

    Integration tests: update components (run-int-tests) (#1351)

---
+++ test/integration/testdata/targets/component-targetmap/commands/download-component.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# SPDX-FileCopyrightText: 2025 SAP SE or an SAP affiliate company and Gardener contributors
+#
+# SPDX-License-Identifier: Apache-2.0
+
+
+COMMAND_DIR="$(dirname $0)"
+HACK_DIR="${COMMAND_DIR}/../../../hack"
+
+source "${HACK_DIR}/settings"
+"${HACK_DIR}/download-component.sh" "${COMMAND_DIR}/component-constructor.yaml" "$REPO_BASE_URL_INTEGRATION_TESTS"
